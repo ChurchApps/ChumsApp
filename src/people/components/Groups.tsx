fe61962839e588cbfaf16129201cfe1978d02ba2
pranavmalvawala <pranavmalvawala@gmail.com>
2021-02-08 08:18:20 +0530

    Show messages when there is no data to display

--- src/people/components/Groups.tsx
+++ src/people/components/Groups.tsx
@@ -13,12 +13,14 @@ export const Groups: React.FC<Props> = (props) => {
     }, [props.personId]);
 
     const items = [];
-    if (groupMembers !== null) {
+    if (groupMembers !== null && groupMembers.length !== 0) {
         for (var i = 0; i < groupMembers.length; i++) {
             var gm = groupMembers[i];
             items.push(<tr key={gm.id}><td><i className="fas fa-list"></i> <Link to={"/groups/" + gm.groupId}>{gm.group.name}</Link></td></tr>);
         }
+    } else {
+        items.push(<tr key="0"><td>Not part of any group yet.</td></tr>);
     }
-
+		console.log('items', items);
     return <DisplayBox headerIcon="fas fa-list" headerText="Groups"><Table size="sm"><tbody>{items}</tbody></Table></DisplayBox>
 }
