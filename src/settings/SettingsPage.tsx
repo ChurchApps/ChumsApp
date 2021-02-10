6a1cddd63d659f3a62e41e54d2ea13aeb05bd802
pranavmalvawala <pranavmalvawala@gmail.com>
2021-02-10 23:38:02 +0530

    Update attendance Api permissions

--- src/settings/SettingsPage.tsx
+++ src/settings/SettingsPage.tsx
@@ -8,7 +8,7 @@ export const SettingsPage = () => {
         if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key={result.length - 2} href="/settings/import" icon="fas fa-upload" text="Import Data" />);
         if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key={result.length - 1} href="/settings/export" icon="fas fa-download" text="Export Data" />);
         if (UserHelper.checkAccess(Permissions.accessApi.roles.view)) result.push(<BigLinkButton key={result.length} href="/settings/roles" icon="fas fa-lock" text="Manage Permissions" />);
-        if (UserHelper.checkAccess(Permissions.attendanceApi.admin.editSettings)) result.push(<BigLinkButton key={result.length - 3} href="/settings/church" icon="fas fa-church" text="Church Settings" />);
+        if (UserHelper.checkAccess(Permissions.attendanceApi.settings.edit)) result.push(<BigLinkButton key={result.length - 3} href="/settings/church" icon="fas fa-church" text="Church Settings" />);
         return result;
     }
 
