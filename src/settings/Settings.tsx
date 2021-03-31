aba3bfe775f0f7817c43f89e56078c437a1565be
pranavmalvawala <pranavmalvawala@gmail.com>
2021-03-31 11:48:47 +0530

    remove roles related code

--- src/settings/Settings.tsx
+++ src/settings/Settings.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { RolesPage } from "./RolesPage"
-import { RolePage } from "./RolePage"
 import { ExportPage } from "./ExportPage"
 import { ImportPage } from "./ImportPage"
 import { SettingsPage } from "./SettingsPage"
@@ -10,11 +8,9 @@ import { ChurchPage } from "./ChurchPage"
 export const Settings: React.FC = () => {
     return (
         <Switch>
-            <Route path="/settings/roles/:id" component={RolePage}></Route>
             <Route path="/settings/church"><ChurchPage /></Route>
             <Route path="/settings/import"><ImportPage /></Route>
             <Route path="/settings/export"><ExportPage /></Route>
-            <Route path="/settings/roles"><RolesPage /></Route>
             <Route path="/settings"><SettingsPage /></Route>
         </Switch>
     );
