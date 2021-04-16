3b17599eac1eadf6597063df237de739a4b4f810
pranavmalvawala <pranavmalvawala@gmail.com>
2021-04-16 14:56:04 +0530

    Remove supportEmail usage from every place

--- src/Forgot.tsx
+++ src/Forgot.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { ForgotPage } from "./appBase/pageComponents/ForgotPage";
-import { EnvironmentHelper } from './helpers'
 
 export const Forgot = () => {
-    return (<ForgotPage supportEmail={EnvironmentHelper.supportEmail} />)
+    return (<ForgotPage />)
 }
