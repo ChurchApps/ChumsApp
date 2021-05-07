07d990ce90ac3b8caa4df6936d771b3853e04073
pranavmalvawala <pranavmalvawala@gmail.com>
2021-05-07 22:04:31 +0530

    code formatting

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import { useCookies } from 'react-cookie';
+import { useCookies } from "react-cookie";
 import { ApiHelper, UserHelper, PersonInterface } from "./components";
 import { Authenticated } from "./Authenticated";
 import UserContext from "./UserContext";
