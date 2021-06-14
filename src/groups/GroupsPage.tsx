fb38600c7c7a55712d97368ee0180d51d77538d8
Jeremy Zongker <jeremy@trilitech.com>
2021-06-14 21:14:01 +0000

    Added loading to groups page

--- src/groups/GroupsPage.tsx
+++ src/groups/GroupsPage.tsx
@@ -1,18 +1,10 @@
 import React from "react";
-import {
-  ApiHelper,
-  DisplayBox,
-  GroupInterface,
-  GroupAdd,
-  UserHelper,
-  ExportLink,
-  Permissions,
-} from "./components";
+import { ApiHelper, DisplayBox, GroupInterface, GroupAdd, UserHelper, ExportLink, Permissions, Loading } from "./components";
 import { Link } from "react-router-dom";
 import { Row, Col, Table } from "react-bootstrap";
 
 export const GroupsPage = () => {
-  const [groups, setGroups] = React.useState<GroupInterface[]>([]);
+  const [groups, setGroups] = React.useState<GroupInterface[]>(null);
   const [showAdd, setShowAdd] = React.useState(false);
 
   const getEditContent = () => {
@@ -21,14 +13,7 @@ export const GroupsPage = () => {
       return (
         <>
           <ExportLink data={groups} spaceAfter={true} filename="groups.csv" />{" "}
-          <a
-            href="about:blank"
-            data-cy="add-button"
-            onClick={(e: React.MouseEvent) => {
-              e.preventDefault();
-              setShowAdd(true);
-            }}
-          >
+          <a href="about:blank" data-cy="add-button" onClick={(e: React.MouseEvent) => { e.preventDefault(); setShowAdd(true); }}>
             <i className="fas fa-plus"></i>
           </a>
         </>
@@ -39,6 +24,7 @@ export const GroupsPage = () => {
     setShowAdd(false);
     loadData();
   };
+
   const loadData = () => {
     ApiHelper.get("/groups", "MembershipApi").then((data) => {
       setGroups(data);
@@ -58,16 +44,10 @@ export const GroupsPage = () => {
     let lastCat = "";
     for (let i = 0; i < groups.length; i++) {
       let g = groups[i];
-      let cat
-        = g.categoryName !== lastCat ? (
-          <>
-            <i className="far fa-folder"></i> {g.categoryName}
-          </>
-        ) : (
-          <></>
-        );
-      let memberCount
-        = g.memberCount === 1 ? "1 person" : g.memberCount.toString() + " people";
+      let cat = g.categoryName !== lastCat
+        ? (<><i className="far fa-folder"></i> {g.categoryName}</>)
+        : (<></>);
+      let memberCount = g.memberCount === 1 ? "1 person" : g.memberCount.toString() + " people";
       rows.push(
         <tr key={g.id}>
           <td>{cat}</td>
@@ -93,33 +73,28 @@ export const GroupsPage = () => {
     return rows;
   }
 
-  let addBox = showAdd ? (
-    <GroupAdd updatedFunction={handleAddUpdated} />
-  ) : (
-    <></>
-  );
+  let addBox = showAdd ? (<GroupAdd updatedFunction={handleAddUpdated} />) : (<></>);
+
+  const getTable = () => {
+    if (!groups) return <Loading />
+    else return (<Table>
+      <thead>{getTableHeader()}</thead>
+      <tbody>{getRows()}</tbody>
+    </Table>);
+  }
 
   return (
     <>
-      <h1>
-        <i className="fas fa-list"></i> Groups
-      </h1>
+      <h1><i className="fas fa-list"></i> Groups</h1>
       <Row>
         <Col lg={8}>
-          <DisplayBox
-            id="groupsBox"
-            headerIcon="fas fa-list"
-            headerText="Groups"
-            editContent={getEditContent()}
-          >
-            <Table>
-              <thead>{getTableHeader()}</thead>
-              <tbody>{getRows()}</tbody>
-            </Table>
+          <DisplayBox id="groupsBox" headerIcon="fas fa-list" headerText="Groups" editContent={getEditContent()}>
+            {getTable()}
           </DisplayBox>
         </Col>
         <Col lg={4}>{addBox}</Col>
       </Row>
     </>
   );
+
 };
