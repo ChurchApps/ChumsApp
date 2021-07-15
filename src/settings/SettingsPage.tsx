83187509c157a5abf4037b82688a4fc5554c3377
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-07-15 10:14:06 +0530

    refactor creating accountsapp link

--- src/settings/SettingsPage.tsx
+++ src/settings/SettingsPage.tsx
@@ -1,23 +1,17 @@
 import React from "react";
-import { UserHelper, BigLinkButton, Permissions, ChurchAppInterface, ApiHelper, ArrayHelper, EnvironmentHelper } from "./components";
+import { UserHelper, BigLinkButton, Permissions, ChurchAppInterface, ArrayHelper } from "./components";
 import { Row } from "react-bootstrap";
 
 export const SettingsPage = () => {
   const [churchApps, setChurchApps] = React.useState<ChurchAppInterface[]>([]);
 
-  const getLoginLink = (church: ChurchAppInterface): string => {
-    const jwt = ApiHelper.getConfig("AccessApi").jwt;
-
-    return `${EnvironmentHelper.AccountsAppUrl}/login/?jwt=${jwt}&returnUrl=/churches/${church.churchId.toString()}/CHUMS`;
-  }
-
   const getLinks = () => {
-    const accessMangementApp = ArrayHelper.getOne(churchApps, "appName", "AccessManagement");
-
+    const accessMangementApp: ChurchAppInterface = ArrayHelper.getOne(churchApps, "appName", "AccessManagement");
+    const returnUrl = `/churches/${accessMangementApp?.churchId.toString()}/manage`;
     let result = [];
     if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key="import" href="/settings/import" icon="fas fa-upload" text="Import Data" />);
     if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key="export" href="/settings/export" icon="fas fa-download" text="Export Data" />);
-    if (accessMangementApp) result.push(<BigLinkButton key="managePermissions" outsideLink={true} href={getLoginLink(accessMangementApp)} icon="fas fa-lock" text="Manage Permissions" />);
+    if (accessMangementApp) result.push(<BigLinkButton key="managePermissions" outsideLink={true} href={UserHelper.goToAccountsApp(returnUrl)} icon="fas fa-lock" text="Manage Permissions" />);
     if (UserHelper.checkAccess(Permissions.attendanceApi.settings.edit)) result.push(<BigLinkButton key="churchSettings" href="/settings/church" icon="fas fa-church" text="Church Settings" />);
     return result;
   }
