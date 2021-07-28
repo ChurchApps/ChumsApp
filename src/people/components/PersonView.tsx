18dd57793c167c482ed34f18949a44d73a227bf3
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-07-28 09:47:07 +0530

    add accessibility attributes

--- src/people/components/PersonView.tsx
+++ src/people/components/PersonView.tsx
@@ -50,7 +50,7 @@ export const PersonView = ({ id, person, editFunction, updatedFunction }: Props)
 
       return (<Row>
         <Col xs={3}>
-          <img src={PersonHelper.getPhotoUrl(person)} className="img-fluid profilePic" id="imgPreview" alt="avatar" />
+          <img src={PersonHelper.getPhotoUrl(person)} className="img-fluid profilePic" aria-label="personImage" id="imgPreview" alt="avatar" />
         </Col>
         <Col xs={9}>
           <h2>{person?.name.display}</h2>
