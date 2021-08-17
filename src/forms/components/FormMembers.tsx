fe7a9c4796cd2c9009f9d46721edbae88a1ba600
Jared Martin <jared@livechurchsolutions.org>
2021-08-17 13:00:09 -0500

    Update form members edit

--- src/forms/components/FormMembers.tsx
+++ src/forms/components/FormMembers.tsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
-import { Col, Row, Table } from "react-bootstrap";
+import { Button, ButtonGroup, Col, Row, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import { DisplayBox, FormInterface, PersonAdd, PersonInterface } from ".";
-import { MemberPermissionInterface, PersonHelper } from "../../helpers";
+import { DisplayBox, PersonAdd, PersonInterface } from ".";
+import { ApiHelper, MemberPermissionInterface, PersonHelper } from "../../helpers";
 
 interface Props { formId: string }
 
 export const FormMembers: React.FC<Props> = (props) => {
+  const [filterList, setFilterList] = useState<string[]>([]);
   const [formMembers, setFormMembers] = useState<MemberPermissionInterface[]>([]);
+
+  const loadData = () => {
+    ApiHelper.get("/memberpermissions/form/" + props.formId, "MembershipApi").then(results => {
+      let filterMembers: string[] = [];
+      results.forEach((member: MemberPermissionInterface) => filterMembers.push(member.memberId));
+      setFilterList(filterMembers);
+      setFormMembers(results);
+    });
+  }
 
   const addPerson = (p: PersonInterface) => {
     const newMember = {
@@ -20,22 +30,31 @@ export const FormMembers: React.FC<Props> = (props) => {
     let fm = [...formMembers];
     fm.push(newMember);
     setFormMembers(fm);
+    ApiHelper.post("/memberpermissions", fm, "MembershipApi");
+    updateFilterList(p.id, "add");
   }
 
-  const handleSetAdmin = (personId: string) => {
+  const handleActionChange = (personId: string) => {
+    // let member = formMembers.find(member => member.memberId === personId);
+    // member.action = "admin";
+    let member;
     let fm = [...formMembers];
     fm.map((p: MemberPermissionInterface) => {
-      if (p.memberId === personId) p.action = "admin"
+      if (p.memberId === personId) {
+        p.action = "admin";
+        member = p;
+      }
     });
+    ApiHelper.post("/memberpermissions", [member], "MembershipApi");
     setFormMembers(fm);
   }
 
   const handleRemoveMember = (personId: string) => {
+    updateFilterList(personId, "remove");
     let fm = [...formMembers];
-    console.log(fm, personId);
     fm = fm.filter((p: MemberPermissionInterface) => p.memberId !== personId);
-    console.log(fm);
     setFormMembers(fm);
+    ApiHelper.delete("/memberpermissions/member/" + personId, "MembershipApi");
   }
 
   const getRows = () => {
@@ -43,11 +62,17 @@ export const FormMembers: React.FC<Props> = (props) => {
     let canEdit = props.formId || true;
     let rows: JSX.Element[] = [];
     formMembers.forEach(fm => {
-      let makeAdminLink = (canEdit && fm.action === "view") ? <a href="about:blank" onClick={(e) => { e.preventDefault(); handleSetAdmin(fm.memberId); }} data-index={fm.id}>Make Admin</a> : <></>
+      // let makeAdminLink = (canEdit && fm.action === "view") ? <a href="about:blank" onClick={(e) => { e.preventDefault(); handleSetAdmin(fm.memberId); }} data-index={fm.id}>Make Admin</a> : <></>
+      // );
       rows.push(
         <tr key={fm.memberId}>
           <td><Link to={"/people/" + fm.memberId}>{fm.personName}</Link></td>
-          <td>{makeAdminLink}</td>
+          <td>
+            <ButtonGroup size="sm">
+              <Button variant={fm.action === "admin" ? "primary" : "outline-primary"} onClick={(e) => { handleActionChange(fm.memberId) }}>Admin</Button>
+              <Button variant={fm.action === "view" ? "primary" : "outline-primary"} onClick={(e) => { handleActionChange(fm.memberId) }}>View Only</Button>
+            </ButtonGroup>
+          </td>
           <td>{<a href="about:blank" onClick={(e) => { e.preventDefault(); handleRemoveMember(fm.memberId); }} className="text-danger"><i className="fas fa-user-times"></i> Remove</a>}</td>
         </tr>
       );
@@ -68,6 +93,15 @@ export const FormMembers: React.FC<Props> = (props) => {
     </Table>
   );
 
+  const updateFilterList = (id: string, action: string) => {
+    let fl = [...filterList];
+    if (action === "add") fl.push(id);
+    if (action === "remove") fl = fl.filter(memberId => memberId !== id);
+    setFilterList(fl);
+  }
+
+  React.useEffect(loadData, [props.formId]);
+
   return (
     <>
       <Row>
@@ -78,7 +112,7 @@ export const FormMembers: React.FC<Props> = (props) => {
         </Col>
         <Col lg={4}>
           <DisplayBox headerText="Add Person" headerIcon="fas fa-users">
-            <PersonAdd getPhotoUrl={PersonHelper.getPhotoUrl} addFunction={addPerson} />
+            <PersonAdd getPhotoUrl={PersonHelper.getPhotoUrl} addFunction={addPerson} filterList={filterList} />
           </DisplayBox>
         </Col>
       </Row>
