5d3500df9d62fb5a2dc0a412eea39cb96ee0059c
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-09-18 17:24:56 +0530

    fix manage church button (#141)

--- src/helpers/UserHelper.ts
+++ src/helpers/UserHelper.ts
@@ -1,18 +1,7 @@
 import { UserHelper as BaseUserHelper, ApiHelper } from "../appBase/helpers"
-import { PersonInterface, ChurchAppInterface, EnvironmentHelper } from ".";
+import { PersonInterface, EnvironmentHelper } from ".";
 export class UserHelper extends BaseUserHelper {
     static person: PersonInterface;
-    static churchApps: ChurchAppInterface[];
-
-    static getChurchApps = async () => {
-      if (UserHelper.churchApps) {
-        return UserHelper.churchApps;
-      }
-
-      const apps = await ApiHelper.get("/churchApps/", "AccessApi");
-      UserHelper.churchApps = apps;
-      return apps
-    }
 
     static goToAccountsApp = (returnUrl: string) => {
       const jwt = ApiHelper.getConfig("AccessApi").jwt;
--- src/settings/SettingsPage.tsx
+++ src/settings/SettingsPage.tsx
@@ -1,24 +1,18 @@
 import React from "react";
-import { UserHelper, BigLinkButton, Permissions, ChurchAppInterface, ArrayHelper } from "./components";
+import { UserHelper, BigLinkButton, Permissions } from "./components";
 import { Row } from "react-bootstrap";
 
 export const SettingsPage = () => {
-  const [churchApps, setChurchApps] = React.useState<ChurchAppInterface[]>([]);
 
   const getLinks = () => {
-    const accessMangementApp: ChurchAppInterface = ArrayHelper.getOne(churchApps, "appName", "AccessManagement");
-    const returnUrl = `/churches/${accessMangementApp?.churchId.toString()}/manage`;
+    const returnUrl = `/${UserHelper.currentChurch.id}/manage`;
     let result = [];
     if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key="import" href="/settings/import" icon="fas fa-upload" text="Import Data" />);
     if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key="export" href="/settings/export" icon="fas fa-download" text="Export Data" />);
-    if (accessMangementApp) result.push(<BigLinkButton key="managePermissions" outsideLink={true} href={UserHelper.goToAccountsApp(returnUrl)} icon="fas fa-lock" text="Manage Permissions" />);
+    if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key="managePermissions" outsideLink={true} href={UserHelper.goToAccountsApp(returnUrl)} icon="fas fa-lock" text="Manage Permissions" />);
     if (UserHelper.checkAccess(Permissions.attendanceApi.settings.edit)) result.push(<BigLinkButton key="churchSettings" href="/settings/church" icon="fas fa-church" text="Church Settings" />);
     return result;
   }
-
-  React.useEffect(() => {
-    UserHelper.getChurchApps().then(apps => { setChurchApps(apps) });
-  }, [])
 
   return (
     <>
