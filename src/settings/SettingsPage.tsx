44a3601e8ecfea39eb815b39cbdaaf96d7690a20
Pranav Malvawala <pranavmalvawala@gmail.com>
2021-09-28 17:25:26 +0530

    use createAppUrl helper

--- src/settings/SettingsPage.tsx
+++ src/settings/SettingsPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { UserHelper, BigLinkButton, Permissions } from "./components";
+import { UserHelper, BigLinkButton, Permissions, EnvironmentHelper } from "./components";
 import { Row } from "react-bootstrap";
 
 export const SettingsPage = () => {
@@ -9,7 +9,7 @@ export const SettingsPage = () => {
     let result = [];
     if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key="import" href="/settings/import" icon="fas fa-upload" text="Import Data" />);
     if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key="export" href="/settings/export" icon="fas fa-download" text="Export Data" />);
-    if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key="managePermissions" outsideLink={true} href={UserHelper.goToAccountsApp(returnUrl)} icon="fas fa-lock" text="Manage Permissions" />);
+    if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key="managePermissions" outsideLink={true} href={UserHelper.createAppUrl(EnvironmentHelper.AccountsAppUrl, returnUrl)} icon="fas fa-lock" text="Manage Permissions" />);
     if (UserHelper.checkAccess(Permissions.attendanceApi.settings.edit)) result.push(<BigLinkButton key="churchSettings" href="/settings/church" icon="fas fa-church" text="Church Settings" />);
     return result;
   }
