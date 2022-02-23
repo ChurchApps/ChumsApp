18d89b645fd1f1c5ada7f5c02f5e6b0422577b6a
nicoleparisis <nicole.parisis@giantmachines.com>
2022-02-23 18:33:47 -0500

    Trim headers when reading csv so they are not strings

--- src/helpers/UploadHelper.ts
+++ src/helpers/UploadHelper.ts
@@ -50,7 +50,10 @@ export class UploadHelper {
 
   static readCsvString(csv: string) {
     let result = [];
-    let data = Papa.parse(csv, { header: true });
+    let data = Papa.parse(csv, { header: true,
+      transformHeader:function(h) {
+        return h.trim();
+      } });
     for (let i = 0; i < data.data.length; i++) {
       let r: any = this.getStrippedRecord(data.data[i]);
       result.push(r);
@@ -65,7 +68,6 @@ export class UploadHelper {
         let result = [];
         let csv = reader.result.toString();
         let data = Papa.parse(csv, { header: true });
-
         for (let i = 0; i < data.data.length; i++) {
           let r: any = this.getStrippedRecord(data.data[i]);
           result.push(r);
