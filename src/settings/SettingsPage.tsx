e8943f97ddc0a927488fd1432332ccd0d968eff6
nicoleparisis <nicole.parisis@giantmachines.com>
2022-03-18 20:36:00 -0400

    add new links for import and export

--- src/settings/SettingsPage.tsx
+++ src/settings/SettingsPage.tsx
@@ -7,6 +7,8 @@ export const SettingsPage = () => {
   const getLinks = () => {
     const returnUrl = `/${UserHelper.currentChurch.id}/manage`;
     let result = [];
+    if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key="import" href="https://transfer.chums.org/" icon="fas fa-upload" text="Import Data" outsideLink={true} />);
+    if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key="export" href="https://transfer.chums.org/" icon="fas fa-download" text="Export Data" outsideLink={true} />);
     if (UserHelper.checkAccess(Permissions.accessApi.settings.edit)) result.push(<BigLinkButton key="managePermissions" outsideLink={true} href={UserHelper.createAppUrl(EnvironmentHelper.AccountsAppUrl, returnUrl)} icon="fas fa-lock" text="Manage Permissions" />);
     if (UserHelper.checkAccess(Permissions.attendanceApi.settings.edit)) result.push(<BigLinkButton key="churchSettings" href="/settings/church" icon="fas fa-church" text="Church Settings" />);
     return result;
