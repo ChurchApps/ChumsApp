0ed0c2f3adf746f019822cfc75bee707a728bab2
matt <matt@grapheene.net>
2022-04-13 13:13:45 -0700

    Fix lint errors

--- src/UserContext.tsx
+++ src/UserContext.tsx
@@ -2,37 +2,37 @@ import React from "react"
 import {PersonHelper, PersonInterface} from "./helpers";
 
 export interface UserContextInterface {
-    userName: string,
-    setUserName: (userName: string) => void,
-    churchName: string,
-    setChurchName: (churchName: string) => void,
-    profilePicture: string,
-    setProfilePicture: (profilePicture: string) => void,
-    person: PersonInterface | null,
-    setPerson: (person: PersonInterface) => void,
+  userName: string,
+  setUserName: (userName: string) => void,
+  churchName: string,
+  setChurchName: (churchName: string) => void,
+  profilePicture: string,
+  setProfilePicture: (profilePicture: string) => void,
+  person: PersonInterface | null,
+  setPerson: (person: PersonInterface) => void,
 }
 
 const UserContext = React.createContext<UserContextInterface | undefined>(undefined);
 
 interface Props {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 export const UserProvider = ({children}: Props) => {
-    const [userName, setUserName] = React.useState("");
-    const [churchName, setChurchName] = React.useState("");
-    const [profilePicture, setProfilePicture] = React.useState(PersonHelper.getPhotoUrl(null));
-    const [person, setPerson] = React.useState(null);
-    return <UserContext.Provider value={{
-        userName,
-        setUserName,
-        churchName,
-        setChurchName,
-        profilePicture,
-        setProfilePicture,
-        person,
-        setPerson
-    }}>{children} </UserContext.Provider>
+  const [userName, setUserName] = React.useState("");
+  const [churchName, setChurchName] = React.useState("");
+  const [profilePicture, setProfilePicture] = React.useState(PersonHelper.getPhotoUrl(null));
+  const [person, setPerson] = React.useState(null);
+  return <UserContext.Provider value={{
+    userName,
+    setUserName,
+    churchName,
+    setChurchName,
+    profilePicture,
+    setProfilePicture,
+    person,
+    setPerson
+  }}>{children} </UserContext.Provider>
 };
 
 export default UserContext;
--- src/people/components/Household.tsx
+++ src/people/components/Household.tsx
@@ -1,88 +1,83 @@
 import React from "react";
 import {
-    DisplayBox,
-    ApiHelper,
-    HouseholdEdit,
-    UserHelper,
-    PersonInterface,
-    Permissions,
-    UniqueIdHelper,
-    Loading, PersonHelper
+  DisplayBox,
+  ApiHelper,
+  HouseholdEdit,
+  UserHelper,
+  PersonInterface,
+  Permissions,
+  UniqueIdHelper,
+  Loading, PersonHelper
 } from ".";
 import {Link} from "react-router-dom";
 import {Table} from "react-bootstrap";
-import UserContext from "../../UserContext";
 
 interface Props {
-    person: PersonInterface,
-    reload: any
+  person: PersonInterface,
+  reload: any
 }
 
 export const Household: React.FC<Props> = (props) => {
-    const context = React.useContext(UserContext);
-    const [household, setHousehold] = React.useState(null);
-    const [members, setMembers] = React.useState<PersonInterface[]>(null);
-    const [mode, setMode] = React.useState("display");
-    const [photo, setPhoto] = React.useState("");
+  const [household, setHousehold] = React.useState(null);
+  const [members, setMembers] = React.useState<PersonInterface[]>(null);
+  const [mode, setMode] = React.useState("display");
+  const [photo, setPhoto] = React.useState("");
 
-    const handleEdit = () => setMode("edit");
-    const handleUpdate = () => {
-        loadData();
-        loadMembers();
-        setMode("display");
+  const handleEdit = () => setMode("edit");
+  const handleUpdate = () => {
+    loadData();
+    loadMembers();
+    setMode("display");
+  }
+  const loadData = () => {
+    if (!UniqueIdHelper.isMissing(props.person?.householdId)) {
+      ApiHelper.get("/households/" + props?.person.householdId, "MembershipApi")
+        .then(data => setHousehold(data));
     }
-    const loadData = () => {
-        if (!UniqueIdHelper.isMissing(props.person?.householdId)) {
-            ApiHelper.get("/households/" + props?.person.householdId, "MembershipApi")
-                .then(data => setHousehold(data));
-        }
+  }
+  const loadMembers = () => {
+    if (household != null) {
+      ApiHelper.get("/people/household/" + household.id, "MembershipApi").then(data => setMembers(data));
     }
-    const loadMembers = () => {
-        if (household != null) {
-            ApiHelper.get("/people/household/" + household.id, "MembershipApi").then(data => setMembers(data));
-        }
-    }
-    const getEditFunction = () => (UserHelper.checkAccess(Permissions.membershipApi.households.edit)) ? handleEdit : null
+  }
+  const getEditFunction = () => (UserHelper.checkAccess(Permissions.membershipApi.households.edit)) ? handleEdit : null
+  React.useEffect(loadData, [props.person]);
+  React.useEffect(() => {
+    setPhoto(PersonHelper.getPhotoUrl(props.person))
+  }, [props.person]);
+  React.useEffect(loadMembers, [household?.id, props.reload]); //eslint-disable-line
 
-    React.useEffect(()=>{
-        loadData()
-        setPhoto(PersonHelper.getPhotoUrl(props.person))
-    }, [props.person]);
-    React.useEffect(loadMembers, [household?.id, props.reload]); //eslint-disable-line
-
-    const getRows = () => {
-        let rows = [];
-        if (members !== null) {
-            for (let i = 0; i < members.length; i++) {
-                let m = members[i];
-                rows.push(
-                    <tr key={m.id}>
-                        <td><img src={photo} alt="avatar"/></td>
-                        <td><Link to={"/people/" + m.id}>{m.name.display}</Link>
-                            <div>{m.householdRole}</div>
-                        </td>
-                    </tr>
-                );
-            }
-        }
-        return rows;
-    }
-
-    const getTable = () => {
-        if (!members) return <Loading size="sm"/>
-        else return (<Table size="sm" id="household">
-            <tbody>{getRows()}</tbody>
-        </Table>);
-    }
-
-    if (mode === "display") {
-        return (
-            <DisplayBox id="householdBox" headerIcon="fas fa-users" headerText={(household?.name || "") + " Household"}
-                        editFunction={getEditFunction()} ariaLabel="editHousehold">
-                {getTable()}
-            </DisplayBox>
+  const getRows = () => {
+    let rows = [];
+    if (members !== null) {
+      for (let i = 0; i < members.length; i++) {
+        let m = members[i];
+        rows.push(
+          <tr key={m.id}>
+            <td><img src={photo} alt="avatar" /></td>
+            <td><Link to={"/people/" + m.id}>{m.name.display}</Link>
+              <div>{m.householdRole}</div>
+            </td>
+          </tr>
         );
-    } else return <HouseholdEdit household={household} members={members} updatedFunction={handleUpdate}
-                                 person={props.person}/>
+      }
+    }
+    return rows;
+  }
+
+  const getTable = () => {
+    if (!members) return <Loading size="sm" />
+    else return (<Table size="sm" id="household">
+      <tbody>{getRows()}</tbody>
+    </Table>);
+  }
+
+  if (mode === "display") {
+    return (
+      <DisplayBox id="householdBox" headerIcon="fas fa-users" headerText={(household?.name || "") + " Household"} editFunction={getEditFunction()} ariaLabel="editHousehold">
+        {getTable()}
+      </DisplayBox>
+    );
+  } else return <HouseholdEdit household={household} members={members} updatedFunction={handleUpdate} person={props.person} />
 }
 
