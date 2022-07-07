87a518d172beeeeab0b9e2f61ab1d1fa419b6e1e
Jeremy Zongker <jeremy@trilitech.com>
2022-07-07 12:27:27 -0500

    Fixed compiler warning

--- src/dashboard/DashboardPage.tsx
+++ src/dashboard/DashboardPage.tsx
@@ -4,18 +4,16 @@ import { TaskList } from "../tasks/components/TaskList";
 import { PeopleSearch, UserHelper } from "./components";
 import { Groups } from "../people/components";
 
-export const DashboardPage = () => {
-  return (<>
-    <h1><Icon>home</Icon> Chums Dashboard</h1>
-    <Grid container spacing={3}>
-      <Grid item md={8} xs={12}>
-        <PeopleSearch />
-        <Groups personId={UserHelper.person?.id} title="My Groups" />
-      </Grid>
-      <Grid item md={4} xs={12}>
-        <TaskList compact={true} status="Open" />
-      </Grid>
+export const DashboardPage = () => (<>
+  <h1><Icon>home</Icon> Chums Dashboard</h1>
+  <Grid container spacing={3}>
+    <Grid item md={8} xs={12}>
+      <PeopleSearch />
+      <Groups personId={UserHelper.person?.id} title="My Groups" />
     </Grid>
-  </>)
-};
+    <Grid item md={4} xs={12}>
+      <TaskList compact={true} status="Open" />
+    </Grid>
+  </Grid>
+</>);
 
