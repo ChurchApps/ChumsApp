f60c9464ca144931df201bcf06db3a5e4880ddb3
Jeremy Zongker <jeremy@trilitech.com>
2022-11-14 11:31:14 -0600

    Updated to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { EnvironmentHelper } from "./helpers";
 
 EnvironmentHelper.init();
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const root = createRoot(document.getElementById("root"));
+root.render(<React.StrictMode><App /></React.StrictMode>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
