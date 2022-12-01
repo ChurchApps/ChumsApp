96623ae30f8678e4f589cf95f2f8a8b5bafb578a
Jeremy Zongker <jeremy@trilitech.com>
2022-12-01 16:18:30 -0600

    Fixed compiler warnings

--- src/UserContext.tsx
+++ src/UserContext.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { ChurchInterface, LoginUserChurchInterface, PersonInterface, UserContextInterface, UserInterface } from "./helpers";
+import { LoginUserChurchInterface, PersonInterface, UserContextInterface, UserInterface } from "./helpers";
 
 const UserContext = React.createContext<UserContextInterface | undefined>(undefined);
 
