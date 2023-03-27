60d289df7a8acb037c3f0ca9fb6bb071b6ce1ec3
Matthew <Matthewmhuston@gmail.com>
2023-03-26 23:32:34 -0700

    Nametag Notes added still need to fix save

--- src/people/components/PersonEdit.tsx
+++ src/people/components/PersonEdit.tsx
@@ -39,7 +39,7 @@ export function PersonEdit(props: Props) {
   const [person, setPerson] = React.useState<PersonInterface>({
     name: { first: "", last: "", middle: "", nick: "", display: "" },
     contactInfo: { address1: "", address2: "", city: "", state: "", zip: "", email: "", homePhone: "", workPhone: "", mobilePhone: "" },
-    membershipStatus: "", gender: "", birthDate: null, maritalStatus: ""
+    membershipStatus: "", gender: "", birthDate: null, maritalStatus: "", nametagNotes: ""
   });
 
   //const handleKeyDown = (e: React.KeyboardEvent<any>) => { if (e.key === "Enter") { e.preventDefault(); handleSave(); } }
@@ -64,6 +64,7 @@ export function PersonEdit(props: Props) {
       case "membershipStatus": p.membershipStatus = value; break;
       case "gender": p.gender = value; break;
       case "maritalStatus": p.maritalStatus = value; break;
+      case "nametagNotes": p.nametagNotes = value; break;
       case "anniversary": p.anniversary = DateHelper.convertToDate(value); break;
       case "birthDate": p.birthDate = DateHelper.convertToDate(value); break;
       case "photo": p.photo = value; break;
@@ -212,6 +213,11 @@ export function PersonEdit(props: Props) {
                 </Grid>
                 <Grid item md={6} xs={12}>
                   <TextField fullWidth name="contactInfo.email" label="Email" type="email" id="email" value={person.contactInfo.email || ""} onChange={handleChange} />
+                </Grid>
+              </Grid>
+              <Grid container spacing={1}>
+                <Grid item md={12} xs={12}>
+                  <TextField fullWidth name="nametagNotes" label="Name Tag Notes" id="nametagnotes" value={person.nametagNotes || ""} onChange={handleChange} />
                 </Grid>
               </Grid>
             </Grid>
