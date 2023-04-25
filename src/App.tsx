1da82a289a24c4de40cbb8a01a80e4b149e61397
Jeremy Zongker <jeremy@zongker.net>
2023-04-25 13:26:32 -0500

    Added misssing dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { UserProvider } from "./UserContext";
 import { CookiesProvider } from "react-cookie";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import "./appBase/components/markdownEditor/editor.css";
+import {Helmet} from "react-helmet";
+import { EnvironmentHelper } from "./helpers";
 
 const mdTheme = createTheme({
   palette: {
@@ -23,6 +25,26 @@ const App: React.FC = () => (
     <CookiesProvider>
       <ThemeProvider theme={mdTheme}>
         <CssBaseline />
+        <Helmet>
+          {EnvironmentHelper.Common.GoogleAnalyticsTag && (
+            <>
+              <script async src={`https://www.googletagmanager.com/gtag/js?id=${EnvironmentHelper.Common.GoogleAnalyticsTag}`} />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${EnvironmentHelper.Common.GoogleAnalyticsTag}', {
+              page_path: window.location.pathname,
+            });
+          `,
+                }}
+              />
+            </>
+          )}
+
+        </Helmet>
         <Router>
           <Routes>
             <Route path="/*" element={<ControlPanel />} />
