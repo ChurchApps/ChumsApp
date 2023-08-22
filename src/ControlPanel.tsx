f91acfc8ecf2462eae9cc2c4409be11db850d8d9
Jeremy Zongker <jeremy@zongker.net>
2023-08-22 18:48:59 -0500

    Added clientError logging

--- src/ControlPanel.tsx
+++ src/ControlPanel.tsx
@@ -1,28 +1,54 @@
 import React from "react";
 import UserContext from "./UserContext";
 
-import { ApiHelper } from "./components";
+import { ApiHelper, ErrorMessages } from "./components";
 import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { Login } from "./Login";
 
 import { Authenticated } from "./Authenticated";
 import { Logout } from "./Logout";
-import { AnalyticsHelper } from "./appBase/helpers";
+import { AnalyticsHelper, UserHelper } from "./appBase/helpers";
+import { ErrorLogInterface, ErrrorAppDataInterface } from "./appBase/interfaces/Error";
+import { ErrorHelper } from "./appBase/helpers/ErrorHelper";
 
 export const ControlPanel = () => {
+  const [errors, setErrors] = React.useState([]);
 
   const location = (typeof(window) === "undefined") ? null : window.location;
   AnalyticsHelper.init();
   React.useEffect(() => { AnalyticsHelper.logPageView() }, [location]);
 
+  const getErrorAppData = () => {
+    const result: ErrrorAppDataInterface = {
+      churchId: UserHelper.currentUserChurch?.church?.id || "",
+      userId: UserHelper.user?.id || "",
+      originUrl: location?.toString(),
+      application: "CHUMS"
+    }
+    return result;
+  }
+
+  const customErrorHandler = (error: ErrorLogInterface) => {
+    console.log("customErrorHandler", error);
+    switch (error.errorType) {
+      case "401": setErrors(["Access denied when loading " + error.message]); break;
+      case "500": setErrors(["Server error when loading " + error.message]); break;
+    }
+  }
+
+  ErrorHelper.init(getErrorAppData, customErrorHandler);
+
   let user = React.useContext(UserContext).user; //to force rerender on login
   if (user === null) console.log("Church is null");
   return (
-    <Routes>
-      <Route path="/logout" element={<Logout />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/*" element={<RequireAuth><Authenticated /></RequireAuth>} />
-    </Routes>
+    <>
+      <ErrorMessages errors={errors} />
+      <Routes>
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<RequireAuth><Authenticated /></RequireAuth>} />
+      </Routes>
+    </>
   );
 };
 
