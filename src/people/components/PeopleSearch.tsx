0e595d05cdff11fe2eba8dfdcb3de20655b30145
Himali Malvawala <himalimalvawala@gmail.com>
2024-02-16 21:34:07 +0530

    improve: donation and campus filters

--- src/people/components/PeopleSearch.tsx
+++ src/people/components/PeopleSearch.tsx
@@ -39,10 +39,15 @@ export function PeopleSearch(props: Props) {
           const peopleIds = ArrayHelper.getIds(members, "personId");
           result.push({ field: "id", operator: c.operator, value: peopleIds.join(",") });
           break;
-        case "donationMember":
+        case "memberDonations":
           const fundVal = JSON.parse(c.value);
-          const donationMember: FundDonationInterface[] = await ApiHelper.get("/funddonations?fundId=" + fundVal.value, "GivingApi");
-          const memberIds = ArrayHelper.getUniqueValues(donationMember, "donation.personId").filter(f => f !== null);
+          let memberDonations: FundDonationInterface[];
+          if (c.operator === "donatedToAny") {
+            memberDonations = await ApiHelper.get(`/funddonations?startDate=${fundVal[1].from}&endDate=${fundVal[1].to}`, "GivingApi");
+          } else {
+            memberDonations = await ApiHelper.get(`/funddonations?fundId=${fundVal[0].value}&startDate=${fundVal[1].from}&endDate=${fundVal[1].to}`, "GivingApi");
+          }
+          const memberIds = ArrayHelper.getUniqueValues(memberDonations, "donation.personId").filter(f => f !== null);
           result.push({ field: "id", operator: c.operator, value: memberIds.join(",") });
           break;
         case "campus":
@@ -94,12 +99,12 @@ export function PeopleSearch(props: Props) {
     let idx = 0;
     for (let c of conditions) {
       const displayField = c.field.split(/(?=[A-Z])/).map(word => (word.charAt(0).toUpperCase() + word.slice(1))).join(" ");
-      const displayOperator = c.operator.replace("lessThanEqual", "<=").replace("greaterThan", ">").replace("equals", "=").replace("lessThan", "<").replace("greaterThanEqual", ">=").replace("notIn", "not in").replace("donatedTo", "donated to").replace("attenedCampus", "attendees at");
+      const displayOperator = c.operator.replace("lessThanEqual", "<=").replace("greaterThan", ">").replace("equals", "=").replace("lessThan", "<").replace("greaterThanEqual", ">=").replace("notIn", "not in").replace("donatedToAny", "made to").replace("donatedTo", "made to").replace("attenedCampus", "attendees at");
       const index = idx;
       let displayValue = (c.value.indexOf('"value":') > -1) ? JSON.parse(c.value).text : c.value;
-      if (c.field === "campus") {
+      if (c.field === "campus" || c.field === "memberDonations") {
         const parsedValue = JSON.parse(c.value);
-        displayValue = `${parsedValue[0].text} [${parsedValue[1]?.from} - ${parsedValue[1]?.to}]`;
+        displayValue = `${parsedValue[0]?.text} [${parsedValue[1]?.from} - ${parsedValue[1]?.to}]`;
       }
       result.push(<Box key={index} sx={{display: "flex", alignItems: "center"}} mb={1}>
         <a href="about:blank" style={{display: "flex"}} onClick={(e) => { e.preventDefault(); removeCondition(index) }}><Icon sx={{ marginRight: "5px" }}>delete</Icon></a>
