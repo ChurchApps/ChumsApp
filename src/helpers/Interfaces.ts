2aea70fb385026b8dceb67aff854182af7df982b
Jeremy Zongker <jeremy@zongker.net>
2024-04-03 09:50:31 -0500

    Updated assignment interface

--- src/helpers/Interfaces.ts
+++ src/helpers/Interfaces.ts
@@ -1,4 +1,5 @@
 export interface PaymentGatewaysInterface { id?: string, churchId?: string, provider?: string, publicKey?: string, privateKey?: string, payFees?: boolean }
+
 export interface PlanInterface { id?: string, churchId?:string, name?: string, serviceDate?: Date, notes?: string }
 export interface PositionInterface { id?: string, churchId?:string, planId?:string, categoryName?:string, name?: string, count?:number, groupId?:string }
 export interface AssignmentInterface { id?: string, churchId?:string, positionId?:string, personId?:string }
