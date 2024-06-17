d8c96b345fe5c79dd91f842457300aefc65e5c5d
Jeremy Zongker <jeremy@zongker.net>
2024-06-17 12:41:33 -0500

    Localization (#279)
    * Got localization working
    
    * Removed i18n
    
    * Cleaned up locale
    
    * Added labels to a few sample pages.
    
    * Added post install code
    
    * Updated apphelper

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,13 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { EnvironmentHelper } from "./helpers";
 
-EnvironmentHelper.init();
-const root = createRoot(document.getElementById("root"));
-//root.render(<React.StrictMode><App /></React.StrictMode>);
-root.render(<App />);
+EnvironmentHelper.init().then(() => {
+  const root = createRoot(document.getElementById("root"));
+  //root.render(<React.StrictMode><App /></React.StrictMode>);
+  root.render(<App />);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  // If you want your app to work offline and load faster, you can change
+  // unregister() to register() below. Note this comes with some pitfalls.
+  // Learn more about service workers: https://bit.ly/CRA-PWA
+  serviceWorker.unregister();
+});
