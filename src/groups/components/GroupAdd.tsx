b18a35533601eb4e16df72561da3c49701ab0f8b
Kat Zongker <katrina@zongker.net>
2024-06-18 17:46:15 -0500

    GroupAdd

--- src/groups/components/GroupAdd.tsx
+++ src/groups/components/GroupAdd.tsx
@@ -1,6 +1,6 @@
 import { TextField } from "@mui/material";
 import React from "react";
-import { ApiHelper, GroupInterface, InputBox, ErrorMessages } from "@churchapps/apphelper";
+import { ApiHelper, GroupInterface, InputBox, ErrorMessages, Locale } from "@churchapps/apphelper";
 
 interface Props { updatedFunction: () => void, tags: string, categoryName?:string}
 
@@ -33,22 +33,22 @@ export const GroupAdd: React.FC<Props> = (props) => {
 
   const validate = () => {
     const result = [];
-    if (!group.categoryName) result.push("Category name is required.");
-    if (!group.name) result.push("Group name is required.");
+    if (!group.categoryName) result.push(Locale.label("groups.groupAdd.catReq"));
+    if (!group.name) result.push(Locale.label("groups.groupAdd.groupReq"));
     setErrors(result);
     return result.length === 0;
   }
 
-  let label = "Group";
-  if (props.tags==="team") label = "Team";
-  else if (props.tags==="ministry") label = "Ministry";
+  let label = Locale.label("groups.groupAdd.group");
+  if (props.tags==="team") label = Locale.label("groups.groupAdd.team");
+  else if (props.tags==="ministry") label = Locale.label("groups.groupAdd.ministry");
 
 
   return (
-    <InputBox headerText={"New " + label} headerIcon="group" cancelFunction={handleCancel} saveFunction={handleAdd} saveText="Add" isSubmitting={isSubmitting}>
+    <InputBox headerText={Locale.label("groups.groupAdd.new") + label} headerIcon="group" cancelFunction={handleCancel} saveFunction={handleAdd} saveText="Add" isSubmitting={isSubmitting}>
       <ErrorMessages errors={errors} />
-      {(props.tags==="standard") && <TextField fullWidth={true} label="Category Name" type="text" id="categoryName" name="categoryName" value={group.categoryName} onChange={handleChange} />}
-      <TextField fullWidth={true} label="Name" type="text" id="groupName" name="name" value={group.name} onChange={handleChange} />
+      {(props.tags==="standard") && <TextField fullWidth={true} label={Locale.label("groups.groupAdd.catName")} type="text" id="categoryName" name="categoryName" value={group.categoryName} onChange={handleChange} />}
+      <TextField fullWidth={true} label={Locale.label("groups.groupAdd.name")} type="text" id="groupName" name="name" value={group.name} onChange={handleChange} />
     </InputBox>
   );
 }
