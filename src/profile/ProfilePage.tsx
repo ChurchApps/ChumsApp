c7b3fa5994fb7fa4982d867a5798eae8305ab7de
Kat Zongker <katrina@zongker.net>
2024-06-21 15:21:17 -0500

    ProfilePage

--- src/profile/ProfilePage.tsx
+++ src/profile/ProfilePage.tsx
@@ -1,7 +1,7 @@
 import { Grid, Icon, TextField, Checkbox, Typography, Button, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from "@mui/material";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { InputBox, ApiHelper, ErrorMessages, UserHelper, NotificationPreferenceInterface } from "@churchapps/apphelper"
+import { InputBox, ApiHelper, ErrorMessages, UserHelper, NotificationPreferenceInterface, Locale } from "@churchapps/apphelper"
 
 export const ProfilePage = () => {
   const [password, setPassword] = useState<string>("");
@@ -42,14 +42,14 @@ export const ProfilePage = () => {
         UserHelper.user.lastName = lastName;
         UserHelper.user.email = email;
         UserHelper.person.optedOut = optedOut;
-        alert("Changes saved.");
+        alert(Locale.label("profile.profilePage.saveChange"));
       });
     }
   }
 
   const handleNotificationSave = () => {
     ApiHelper.post("/notificationPreferences", [pref], "MessagingApi").then(() => {
-      alert("Changes saved.");
+      alert(Locale.label("profile.profilePage.saveChange"));
     });
   }
 
@@ -97,19 +97,19 @@ export const ProfilePage = () => {
 
   const validate = () => {
     let errors = [];
-    if (!firstName) errors.push("Please enter firstname");
-    if (!lastName) errors.push("Please enter lastname");
-    if (email === "") errors.push("Please enter an email address.");
-    else if (!validateEmail(email)) errors.push("Please enter a valid email address");
-    if (password !== passwordVerify) errors.push("Passwords do not match.");
-    if (password !== "" && password.length < 8) errors.push("Please enter a password that is at least 8 characters long.");
+    if (!firstName) errors.push(Locale.label("profile.profilePage.firstMsg"));
+    if (!lastName) errors.push(Locale.label("profile.profilePage.lastMsg"));
+    if (email === "") errors.push(Locale.label("profile.profilePage.emailMsg"));
+    else if (!validateEmail(email)) errors.push(Locale.label("profile.profilePage.valEmail"));
+    if (password !== passwordVerify) errors.push(Locale.label("profile.profilePage.passMatch"));
+    if (password !== "" && password.length < 8) errors.push(Locale.label("profile.profilePage.passLong"));
 
     setErrors(errors);
     return errors.length === 0;
   }
 
   const handleAccountDelete = () => {
-    if (window.confirm("Are you sure you wish to permanently delete your account?")) {
+    if (window.confirm(Locale.label("profile.profilePage.confirmMsg"))) {
       ApiHelper.delete("/users", "MembershipApi").then(() => {
         navigate("/logout", { replace: true })
       })
@@ -120,52 +120,52 @@ export const ProfilePage = () => {
 
   return (
     <>
-      <h1><Icon>person</Icon> Edit Profile</h1>
+      <h1><Icon>person</Icon> {Locale.label("profile.profilePage.profEdit")}</h1>
       <ErrorMessages errors={errors} />
-      <InputBox headerText="Edit Profile" saveFunction={handleSave}>
+      <InputBox headerText={Locale.label("profile.profilePage.profEdit")} saveFunction={handleSave}>
         <Grid container spacing={3}>
           <Grid item>
-            <TextField fullWidth type="email" name="email" label="Email" value={email} onChange={handleChange} />
-            <TextField fullWidth name="firstName" label="First Name" value={firstName} onChange={handleChange} />
-            <TextField fullWidth name="lastName" label="Last Name" value={lastName} onChange={handleChange} />
+            <TextField fullWidth type="email" name="email" label={Locale.label("profile.profilePage.email")} value={email} onChange={handleChange} />
+            <TextField fullWidth name="firstName" label={Locale.label("profile.profilePage.firstName")} value={firstName} onChange={handleChange} />
+            <TextField fullWidth name="lastName" label={Locale.label("profile.profilePage.lastName")} value={lastName} onChange={handleChange} />
           </Grid>
           <Grid item>
-            <TextField type="password" fullWidth name="password" label="New password" value={password} onChange={handleChange} />
-            <TextField type="password" fullWidth name="passwordVerify" label="Verify password" value={passwordVerify} onChange={handleChange} />
+            <TextField type="password" fullWidth name="password" label={Locale.label("profile.profilePage.passNew")} value={password} onChange={handleChange} />
+            <TextField type="password" fullWidth name="passwordVerify" label={Locale.label("profile.profilePage.passVer")} value={passwordVerify} onChange={handleChange} />
             <Checkbox name="optedOut" checked={optedOut} onChange={handleChange} />
-            <label htmlFor="optedOut">Directory opt-out</label>
+            <label htmlFor="optedOut">{Locale.label("profile.profilePage.noDirect")}</label>
           </Grid>
         </Grid>
       </InputBox>
 
-      <InputBox headerText="Notification Preferences" saveFunction={handleNotificationSave}>
-        <p>Choose how you would like to receive updates about private messages, conversations, forms and other notifications.</p>
+      <InputBox headerText={Locale.label("profile.profilePage.notifPref")} saveFunction={handleNotificationSave}>
+        <p>{Locale.label("profile.profilePage.notifMsg")}</p>
         <Grid container spacing={3}>
           <Grid item sm={6}>
             <FormControl fullWidth>
-              <InputLabel id="push">Allow Push Notifications</InputLabel>
-              <Select fullWidth name="push" labelId="push" label="Allow Push Notifications" value={pref.allowPush?.toString() || "true"} onChange={handlePrefChange}>
-                <MenuItem value="true">Yes</MenuItem>
-                <MenuItem value="false">No</MenuItem>
+              <InputLabel id="push">{Locale.label("profile.profilePage.notifPush")}</InputLabel>
+              <Select fullWidth name="push" labelId="push" label={Locale.label("profile.profilePage.notifPush")} value={pref.allowPush?.toString() || "true"} onChange={handlePrefChange}>
+                <MenuItem value="true">{Locale.label("profile.profilePage.yes")}</MenuItem>
+                <MenuItem value="false">{Locale.label("profile.profilePage.no")}</MenuItem>
               </Select>
             </FormControl>
           </Grid>
           <Grid item sm={6}>
             <FormControl fullWidth>
-              <InputLabel id="emailFrequency">Email Frequency</InputLabel>
-              <Select fullWidth name="emailFrequency" labelId="emailFrequency" label="Email Frequency" value={pref.emailFrequency || "daily"} onChange={handlePrefChange}>
-                <MenuItem value="never">Never</MenuItem>
-                <MenuItem value="individual">Individual</MenuItem>
-                <MenuItem value="daily">Daily</MenuItem>
+              <InputLabel id="emailFrequency">{Locale.label("profile.profilePage.freqEmail")}</InputLabel>
+              <Select fullWidth name="emailFrequency" labelId="emailFrequency" label={Locale.label("profile.profilePage.freqEmail")} value={pref.emailFrequency || "daily"} onChange={handlePrefChange}>
+                <MenuItem value="never">{Locale.label("profile.profilePage.never")}</MenuItem>
+                <MenuItem value="individual">{Locale.label("profile.profilePage.indiv")}</MenuItem>
+                <MenuItem value="daily">{Locale.label("profile.profilePage.daily")}</MenuItem>
               </Select>
             </FormControl>
           </Grid>
         </Grid>
       </InputBox>
 
-      <InputBox headerText="Account Deletion" saveFunction={null}>
-        <Typography color="GrayText">Careful, these actions are permanent</Typography>
-        <Button variant="outlined" color="error" sx={{ marginTop: 4 }} onClick={handleAccountDelete}>Delete my account</Button>
+      <InputBox headerText={Locale.label("profile.profilePage.accDel")} saveFunction={null}>
+        <Typography color="GrayText">{Locale.label("profile.profilePage.permWarn")}</Typography>
+        <Button variant="outlined" color="error" sx={{ marginTop: 4 }} onClick={handleAccountDelete}>{Locale.label("profile.profilePage.delAcc")}</Button>
       </InputBox>
     </>
   );
