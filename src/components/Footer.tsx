07b04555e1134c73ed3a01029fe4ba81262852c9
Kat Zongker <katrina@zongker.net>
2024-06-21 19:24:35 -0500

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Box } from "@mui/material";
+import { Locale } from "@churchapps/apphelper";
 
 export const Footer: React.FC = () => (
   <div id="footer">
     <Box sx={{textAlign: "center"}}>
       <img src="/images/logo.png" alt="logo" />
-      <p>Phone: 918-994-2638 &nbsp; | &nbsp; support@chums.org</p>
-      <p>2020 © Live Church Solutions. All rights reserved.</p>
+      <p>{Locale.label("components.footer.phone")}: 918-994-2638 &nbsp; | &nbsp; support@chums.org</p>
+      <p>2020 © Live Church Solutions. {Locale.label("components.footer.rights")}</p>
     </Box>
   </div>
 )
