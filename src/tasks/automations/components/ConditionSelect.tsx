ab8a4980400ce6c31285da46c07958bdc4d8cd2b
Kat Zongker <katrina@zongker.net>
2024-06-23 14:59:42 -0500

    ConditionSelect

--- src/tasks/automations/components/ConditionSelect.tsx
+++ src/tasks/automations/components/ConditionSelect.tsx
@@ -1,7 +1,7 @@
 import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
 import React from "react";
 import { ConditionHelper } from "../../components";
-import { ConditionInterface } from "@churchapps/apphelper";
+import { ConditionInterface, Locale } from "@churchapps/apphelper";
 
 interface Props {
   condition: ConditionInterface,
@@ -42,35 +42,35 @@ export const ConditionSelect = (props: Props) => {
 
   const getGender = () => (
     <FormControl fullWidth>
-      <InputLabel>Gender</InputLabel>
-      <Select fullWidth label="Gender" value={props.condition.value || ""} name="value" onChange={handleChange}>
-        <MenuItem value="Unknown">Unknown</MenuItem>
-        <MenuItem value="Male">Male</MenuItem>
-        <MenuItem value="Female">Female</MenuItem>
+      <InputLabel>{Locale.label("tasks.conditionSelect.gender")}</InputLabel>
+      <Select fullWidth label={Locale.label("tasks.conditionSelect.gender")} value={props.condition.value || ""} name="value" onChange={handleChange}>
+        <MenuItem value="Unknown">{Locale.label("tasks.conditionSelect.unknown")}</MenuItem>
+        <MenuItem value="Male">{Locale.label("tasks.conditionSelect.male")}</MenuItem>
+        <MenuItem value="Female">{Locale.label("tasks.conditionSelect.female")}</MenuItem>
       </Select>
     </FormControl>
   )
 
   const getMaritalStatus = () => (
     <FormControl fullWidth>
-      <InputLabel>Marital Status</InputLabel>
-      <Select fullWidth label="Marital Status" value={props.condition.value || ""} name="value" onChange={handleChange}>
-        <MenuItem value="Unknown">Unknown</MenuItem>
-        <MenuItem value="Single">Single</MenuItem>
-        <MenuItem value="Married">Married</MenuItem>
-        <MenuItem value="Divorced">Divorced</MenuItem>
-        <MenuItem value="Widowed">Widowed</MenuItem>
+      <InputLabel>{Locale.label("tasks.conditionSelect.marStat")}</InputLabel>
+      <Select fullWidth label={Locale.label("tasks.conditionSelect.marStat")} value={props.condition.value || ""} name="value" onChange={handleChange}>
+        <MenuItem value="Unknown">{Locale.label("tasks.conditionSelect.unknown")}</MenuItem>
+        <MenuItem value="Single">{Locale.label("tasks.conditionSelect.single")}</MenuItem>
+        <MenuItem value="Married">{Locale.label("tasks.conditionSelect.married")}</MenuItem>
+        <MenuItem value="Divorced">{Locale.label("tasks.conditionSelect.divorced")}</MenuItem>
+        <MenuItem value="Widowed">{Locale.label("tasks.conditionSelect.widowed")}</MenuItem>
       </Select>
     </FormControl>
   )
 
   const getMembershipStatus = () => (
     <FormControl fullWidth>
-      <InputLabel>Membership Status</InputLabel>
-      <Select fullWidth label="Membership Status" value={props.condition.value || ""} name="value" onChange={handleChange}>
-        <MenuItem value="Member">Member</MenuItem>
-        <MenuItem value="Visitor">Visitor</MenuItem>
-        <MenuItem value="Staff">Staff</MenuItem>
+      <InputLabel>{Locale.label("tasks.conditionSelect.memShipStat")}</InputLabel>
+      <Select fullWidth label={Locale.label("tasks.conditionSelect.memShipStat")} value={props.condition.value || ""} name="value" onChange={handleChange}>
+        <MenuItem value="Member">{Locale.label("tasks.conditionSelect.mem")}</MenuItem>
+        <MenuItem value="Visitor">{Locale.label("tasks.conditionSelect.visitor")}</MenuItem>
+        <MenuItem value="Staff">{Locale.label("tasks.conditionSelect.staff")}</MenuItem>
       </Select>
     </FormControl>
   )
@@ -88,10 +88,10 @@ export const ConditionSelect = (props: Props) => {
   console.log(props.condition.operator)
   return <>
     <FormControl fullWidth>
-      <InputLabel>Operator</InputLabel>
-      <Select fullWidth label="Operator" value={props.condition.operator || ""} name="operator" onChange={handleChange}>
-        <MenuItem value="=">is</MenuItem>
-        <MenuItem value="!=">is not</MenuItem>
+      <InputLabel>{Locale.label("tasks.conditionSelect.op")}</InputLabel>
+      <Select fullWidth label={Locale.label("tasks.conditionSelect.op")} value={props.condition.operator || ""} name="operator" onChange={handleChange}>
+        <MenuItem value="=">{Locale.label("tasks.conditionSelect.is")}</MenuItem>
+        <MenuItem value="!=">{Locale.label("tasks.conditionSelect.isNot")}</MenuItem>
       </Select>
     </FormControl>
     {getValueField()}
