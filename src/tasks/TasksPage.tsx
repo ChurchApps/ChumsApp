ed4349b3557f9572b1938956abd2d413d2fff579
Kat Zongker <katrina@zongker.net>
2024-06-23 17:22:29 -0500

    TasksPage

--- src/tasks/TasksPage.tsx
+++ src/tasks/TasksPage.tsx
@@ -9,9 +9,9 @@ export const TasksPage = () => {
   //
   return (<>
     <span style={{ float: "right", paddingTop: 15 }}>
-      <SmallButton icon="settings_suggest" text="Automations" href="/tasks/automations" /> &nbsp;
-      {(status === "Open") && <SmallButton icon="list_alt" text="Show Closed" onClick={() => { setStatus("Closed") }} />}
-      {(status === "Closed") && <SmallButton icon="list_alt" text="Show Open" onClick={() => { setStatus("Open") }} />}
+      <SmallButton icon="settings_suggest" text={Locale.label("tasks.tasksPage.auto")} href="/tasks/automations" /> &nbsp;
+      {(status === "Open") && <SmallButton icon="list_alt" text={Locale.label("tasks.tasksPage.showClosed")} onClick={() => { setStatus("Closed") }} />}
+      {(status === "Closed") && <SmallButton icon="list_alt" text={Locale.label("tasks.tasksPage.showOpen")} onClick={() => { setStatus("Open") }} />}
     </span>
     <h1><Icon>list_alt</Icon> {Locale.label("tasks.tasksPage.tasks")}</h1>
     <TaskList status={status} />
