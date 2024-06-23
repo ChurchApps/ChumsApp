a40ee04e3d49395cca06b48d0eb041c26f63e459
Kat Zongker <katrina@zongker.net>
2024-06-23 17:01:00 -0500

    RoleMembers

--- src/settings/components/RoleMembers.tsx
+++ src/settings/components/RoleMembers.tsx
@@ -39,8 +39,8 @@ export const RoleMembers: React.FC<Props> = (props) => {
 
     for (let i = 0; i < roleMembers.length; i++) {
       const rm = roleMembers[i];
-      const removeLink = (canEdit) ? (<SmallButton icon="delete" color="error" toolTip="Delete" onClick={() => { handleRemove(rm) }} />) : null;
-      const editLink = (canEdit) ? (<SmallButton icon="edit" toolTip="Edit" onClick={() => { props.setSelectedRoleMember(rm.userId) }} />) : null;
+      const removeLink = (canEdit) ? (<SmallButton icon="delete" color="error" toolTip={Locale.label("settings.roleMembers.delete")} onClick={() => { handleRemove(rm) }} />) : null;
+      const editLink = (canEdit) ? (<SmallButton icon="edit" toolTip={Locale.label("settings.roleMembers.edit")} onClick={() => { props.setSelectedRoleMember(rm.userId) }} />) : null;
 
       const { firstName, lastName } = rm.user;
       rows.push(
