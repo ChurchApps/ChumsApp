a491e3015bad6a6ffd78408688e34f56e381b51d
Himali Malvawala <86650000+Himali-Malvawala@users.noreply.github.com>
2024-06-28 14:47:27 +0530

    add: directory approval group field to church settings (#281)

--- src/settings/components/ChurchSettingsEdit.tsx
+++ src/settings/components/ChurchSettingsEdit.tsx
@@ -3,6 +3,7 @@ import { ApiHelper, ChurchInterface, InputBox, ErrorMessages, UserHelper, Permis
 import { GivingSettingsEdit } from "./GivingSettingsEdit";
 import { TextField, Grid } from "@mui/material";
 import { DomainSettingsEdit } from "./DomainSettingsEdit";
+import { DirectoryApproveSettingsEdit } from "./DirectoryApproveSettingsEdit";
 
 interface Props { church: ChurchInterface, updatedFunction: () => void }
 
@@ -88,6 +89,7 @@ export const ChurchSettingsEdit: React.FC<Props> = (props) => {
         </Grid>
       </Grid>
       <TextField fullWidth name="country" label={Locale.label("settings.churchSettingsEdit.country")} value={church?.country || ""} onChange={handleChange} onKeyDown={handleKeyDown} />
+      <DirectoryApproveSettingsEdit churchId={church?.id || ""} saveTrigger={saveTrigger}  />
       {giveSection()}
       <DomainSettingsEdit churchId={church?.id || ""} saveTrigger={saveTrigger} />
 
