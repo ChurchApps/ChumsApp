cc80eae5ae0884b18fabcae78f3f0c57ca007f4b
Himali Malvawala <86650000+Himali-Malvawala@users.noreply.github.com>
2024-08-26 23:10:21 +0530

    add: tax deductible to funds (#293)
    * add: tax deductible to funds
    
    * localize tax deductible
    
    * update churchapps/apphelper

--- src/donations/components/FundEdit.tsx
+++ src/donations/components/FundEdit.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import { ApiHelper, InputBox, FundInterface, ErrorMessages, Locale } from "@churchapps/apphelper";
-import { TextField } from "@mui/material";
+import { Checkbox, FormControlLabel, TextField, Typography } from "@mui/material";
 
 interface Props { fund: FundInterface, updatedFunction: () => void }
 export const FundEdit: React.FC<Props> = (props) => {
-  const [fund, setFund] = useState<FundInterface>({ id: "", name: "" });
+  const [fund, setFund] = useState<FundInterface>({ id: "", name: "", taxDeductible: true });
   const [errors, setErrors] = useState<string[]>([]);
 
   const handleCancel = () => props.updatedFunction();
@@ -31,7 +31,16 @@ export const FundEdit: React.FC<Props> = (props) => {
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     let f = { ...fund };
-    f.name = e.target.value;
+    switch (e.target.name) {
+      case "fundName":
+        f.name = e.target.value;
+        break;
+      case "taxDeductible":
+        f.taxDeductible = e.target.checked;
+        break;
+      default:
+        break;
+    }
     setFund(f);
   }
 
@@ -41,6 +50,8 @@ export const FundEdit: React.FC<Props> = (props) => {
     <InputBox id="fundsBox" headerIcon="volunteer_activism" headerText={Locale.label("common.edit")} cancelFunction={handleCancel} saveFunction={handleSave} deleteFunction={(fund.id === "") ? undefined : handleDelete} help="chums/giving">
       <ErrorMessages errors={errors} />
       <TextField fullWidth name="fundName" label={Locale.label("common.name")} value={fund.name} onChange={handleChange} onKeyDown={handleKeyDown} />
+      <FormControlLabel control={<Checkbox sx={{ marginLeft: "5px" }} checked={fund.taxDeductible} onChange={handleChange} name="taxDeductible" />} label={Locale.label("donations.fundEdit.taxDeductible")} />
+      <Typography sx={{ fontStyle: "italic", fontSize: "12px", marginLeft: "5px" }}>{fund.taxDeductible ? Locale.label("donations.fundEdit.trackDonations") : Locale.label("donations.fundEdit.trackNonDonations")}</Typography>
     </InputBox>
 
   );
