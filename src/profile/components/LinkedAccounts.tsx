e72fecb35b0e07b8404cbc9ddeda4360aca53de8
Jeremy Zongker <jeremy@zongker.net>
2025-03-31 16:50:32 -0500

    Getting access token

--- src/profile/components/LinkedAccounts.tsx
+++ src/profile/components/LinkedAccounts.tsx
@@ -5,22 +5,27 @@ import { Button, Grid } from "@mui/material";
 export const LinkedAccounts = () => {
 
   const [settings, setSettings] = React.useState<SettingInterface[]>([]);
+  const [oauthToken, setOauthToken] = React.useState<string | null>(null);
+  const [oauthTokenSecret, setOauthTokenSecret] = React.useState<string | null>(null);
 
   const loadData = () => {
     ApiHelper.get("/settings/my", "ContentApi").then(data => { setSettings(data); });
   }
 
   const unlinkPraiseCharts = async () => {
-    const token = settings.find(s => s.keyName === "praiseChartsToken");
-    const verifier = settings.find(s => s.keyName === "praiseChartsVerifier");
-    if (verifier) await ApiHelper.delete("/settings/my/" + verifier.id, "ContentApi");
+    const token = settings.find(s => s.keyName === "praiseChartsAccessToken");
+    const secret = settings.find(s => s.keyName === "praiseChartsAccessTokenSecret");
+    if (secret) await ApiHelper.delete("/settings/my/" + secret.id, "ContentApi");
     if (token) await ApiHelper.delete("/settings/my/" + token.id, "ContentApi");
     loadData();
   }
 
   const openOAuthPopup = async () => {
     const returnUrl = "http://localhost:3101/pingback"
-    const { authUrl } = await ApiHelper.get("/songDetails/praiseCharts/authUrl?returnUrl=" + encodeURIComponent(returnUrl), "ContentApi");
+    const { authUrl, oauthToken, oauthTokenSecret } = await ApiHelper.get("/songDetails/praiseCharts/authUrl?returnUrl=" + encodeURIComponent(returnUrl), "ContentApi");
+    setOauthToken(oauthToken);
+    setOauthTokenSecret(oauthTokenSecret);
+
     const popup = window.open(authUrl, 'oauth', 'width=600,height=700');
 
     // Listen for message from popup
@@ -28,29 +33,15 @@ export const LinkedAccounts = () => {
       if (event.origin !== window.location.origin) return;
       const { oauth_token, oauth_verifier } = event.data;
       popup.close();
-      const settings: SettingInterface[] = [
-        { keyName: "praiseChartsToken", value: oauth_token },
-        { keyName: "praiseChartsVerifier", value: oauth_verifier }
-      ]
-      await ApiHelper.post("/settings/my", settings, "ContentApi");
+
+      await ApiHelper.get("/songDetails/praiseCharts/access?verifier=" + encodeURIComponent(oauth_verifier) + "&token=" + encodeURIComponent(oauthToken) + "&secret=" + encodeURIComponent(oauthTokenSecret), "ContentApi");
       loadData();
-      /*
-
-      const result = await fetch('/api/oauth/access-token', {
-        method: 'POST',
-        body: JSON.stringify({ oauth_token, oauth_verifier }),
-        headers: { 'Content-Type': 'application/json' }
-      }).then(r => r.json());
-      */
-
-
-      // Do something with result (store token, update UI, etc.)
     });
   };
 
   useEffect(loadData, []);
 
-  const praiseChartsToken = settings.find(s => s.keyName === "praiseChartsToken")?.value;
+  const praiseChartsAccessToken = settings.find(s => s.keyName === "praiseChartsAccessToken")?.value;
 
   return (
     <>
@@ -61,8 +52,8 @@ export const LinkedAccounts = () => {
             <img src="/images/praisecharts.png" alt="PraiseCharts" style={{ height: 40 }} />
           </Grid>
           <Grid item sm={1} style={{ textAlign: "right" }}>
-            {!praiseChartsToken && (<Button variant="contained" color="primary" onClick={() => openOAuthPopup()}>Link</Button>)}
-            {praiseChartsToken && (<Button variant="contained" color="error" onClick={unlinkPraiseCharts}>Unlink</Button>)}
+            {!praiseChartsAccessToken && (<Button variant="contained" color="primary" onClick={() => openOAuthPopup()}>Link</Button>)}
+            {praiseChartsAccessToken && (<Button variant="contained" color="error" onClick={unlinkPraiseCharts}>Unlink</Button>)}
           </Grid>
         </Grid>
 
