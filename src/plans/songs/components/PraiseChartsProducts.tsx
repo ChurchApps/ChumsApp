9c000ea10add84424d82b3c6022f8bdbd2a0674e
Jeremy Zongker <jeremy@zongker.net>
2025-04-01 09:26:11 -0500

    Improved accordion

--- src/plans/songs/components/PraiseChartsProducts.tsx
+++ src/plans/songs/components/PraiseChartsProducts.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { ApiHelper, ArrayHelper, CurrencyHelper } from "@churchapps/apphelper";
-import { Button, Icon, Table, TableBody, TableCell, TableRow } from "@mui/material";
+import { Button, Grid, Icon, Table, TableBody, TableCell, TableRow } from "@mui/material";
 
 interface Props {
   praiseChartsId: string;
@@ -9,7 +9,7 @@ interface Props {
 export const PraiseChartsProducts = (props: Props) => {
 
   const [products, setProducts] = React.useState<any[]>([]);
-  const [expandedSku, setExpandedSku] = React.useState<string>("");
+  const [expandedSkus, setExpandedSkus] = React.useState<string[]>([]);
 
   const loadData = async () => {
     if (props.praiseChartsId) {
@@ -20,39 +20,40 @@ export const PraiseChartsProducts = (props: Props) => {
     }
   }
 
-  const findRecursive = (productTree: any[], product: any) => {
-    let result = productTree.find((p: any) => p.sku === product.sku);
-    if (!result) {
-      productTree.forEach((p: any) => {
-        if (p.children) {
-          result = findRecursive(p.children, product);
-        }
-      });
-    }
-    return result;
-  }
-
-  const addChildren = (product: any, products: any[]) => {
+  const addChildren = (product: any, allProducts: any[]) => {
     if (product.child_products) {
       product.children = [];
       product.child_products.forEach((child: any) => {
-        const c = products.find((p: any) => p.sku === child.sku);
+        const c = allProducts.find((p: any) => p.sku === child.sku);
         if (c) product.children.push(c);
-        addChildren(c, products);
+        addChildren(c, allProducts);
       });
     }
   }
 
-  const buildTree = (products: any[]) => {
-    const result: any[] = [];
+  const buildTree = (allProducts: any[]) => {
+    const rootExcludedSkus: string[] = [];
     products.forEach((product: any) => {
-      let found = findRecursive(result, product);
-      if (!found) {
+      if (product.child_products) {
+        product.child_products.forEach((child: any) => {
+          rootExcludedSkus.push(child.sku);
+        });
+      }
+    });
+
+    console.log("Root Excluded Skus", rootExcludedSkus);
+
+    const result: any[] = [];
+    allProducts.forEach((product: any) => {
+      if (!rootExcludedSkus.includes(product.sku)) {
         result.push(product);
-        addChildren(product, products);
+      }
+      if (product.child_products) {
+        addChildren(product, allProducts);
       }
     });
     return result;
+
   }
 
   useEffect(() => { loadData() }, [props.praiseChartsId]) //eslint-disable-line react-hooks/exhaustive-deps
@@ -68,14 +69,23 @@ export const PraiseChartsProducts = (props: Props) => {
 
   const getProductRow = (product: any, indent: number) => {
     let a = 0;
-    let result = <TableRow key={product.sku}>
-      <TableCell style={{ paddingLeft: indent * 20 }}><a href="about:blank" onClick={(e) => { e.preventDefault(); setExpandedSku(product.sku) }}>{product.children?.length > 0 && "C"}</a></TableCell>
-      <TableCell style={{ paddingLeft: indent * 20 }}>{product.name}</TableCell>
-      <TableCell>{product.file_type}</TableCell>
-      <TableCell>{getPriceButton(product)}</TableCell>
-    </TableRow>
+    let expand = <></>;
+    if (product.children?.length > 0) {
+      if (expandedSkus.includes(product.sku)) expand = <a href="about:blank" onClick={(e) => { e.preventDefault(); setExpandedSkus(expandedSkus.filter(s => s !== product.sku)) }}>
+        <Icon>expand_less</Icon>
+      </a>
+      else expand = <a href="about:blank" onClick={(e) => { e.preventDefault(); setExpandedSkus([...expandedSkus, product.sku]) }}>
+        <Icon>expand_more</Icon>
+      </a>
+    }
+    let result = <Grid container key={product.sku} style={{ marginBottom: 4 }}>
+      <Grid item xs={1}>{expand}</Grid>
+      <Grid item xs={4} style={{ paddingLeft: indent * 20 }}>{product.name}</Grid>
+      <Grid item xs={1}>{product.file_type}</Grid>
+      <Grid item xs={2}>{getPriceButton(product)}</Grid>
+    </Grid>
 
-    if (product.sku === expandedSku) {
+    if (expandedSkus.includes(product.sku)) {
       const items = [result];
       product.children?.forEach((child: any) => {
         items.push(getProductRow(child, indent + 1) as JSX.Element);
@@ -86,12 +96,8 @@ export const PraiseChartsProducts = (props: Props) => {
     return result;
   }
 
-  return (
-    <Table>
-      <TableBody>
-        {products.map((product: any) => getProductRow(product, 0))}
-      </TableBody>
-
-    </Table>)
+  return (<>
+    {products.map((product: any) => getProductRow(product, 0))}
+  </>)
 }
 
