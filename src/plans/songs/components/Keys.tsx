fe601c4cc9ee7f6cd67d402eae0e93756fab3491
Jeremy Zongker <jeremy@zongker.net>
2025-04-04 14:03:57 -0500

    Added manual links to keys

--- src/plans/songs/components/Keys.tsx
+++ src/plans/songs/components/Keys.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from "react";
 import { ArrangementInterface, ArrangementKeyInterface, SongDetailInterface } from "../../../helpers";
-import { ApiHelper, ArrayHelper, DisplayBox } from "@churchapps/apphelper";
-import { Alert, Box, Button, Icon, Tab, Tabs } from "@mui/material";
+import { ApiHelper, ArrayHelper, DisplayBox, LinkInterface, Locale, SmallButton } from "@churchapps/apphelper";
+import { Alert, Box, Button, Icon, Menu, MenuItem, Tab, Tabs } from "@mui/material";
 import { PraiseChartsProducts } from "./PraiseChartsProducts";
 import { KeyEdit } from "./KeyEdit";
 import { PraiseChartsHelper } from "../../../helpers/PraiseChartsHelper";
+import { LinkEdit } from "./LinkEdit";
 
 interface Props {
   arrangement: ArrangementInterface;
@@ -16,9 +17,19 @@ export const Keys = (props: Props) => {
   const [keys, setKeys] = React.useState<ArrangementKeyInterface[]>([])
   const [selectedKey, setSelectedKey] = React.useState<ArrangementKeyInterface>(null);
   const [editKey, setEditKey] = React.useState<ArrangementKeyInterface>(null);
+  const [editLink, setEditLink] = React.useState<LinkInterface>(null);
   const [products, setProducts] = React.useState<any[]>([]);
+  const [links, setLinks] = React.useState<LinkInterface[]>([]);
   const [showImport, setShowImport] = React.useState(false);
   const [canImportLyrics, setCanImportLyrics] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (e: React.MouseEvent) => {
+    setAnchorEl(e.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleTabChange = (event: React.SyntheticEvent, newValue: string) => {
     if (newValue === "add") setEditKey({ arrangementId: props.arrangement.id, keySignature: "C", shortDescription: "Default" });
@@ -47,8 +58,12 @@ export const Keys = (props: Props) => {
     }
   }
 
+  const loadLinks = () => {
+    if (selectedKey) ApiHelper.get("/links?category=arrangementKey_" + selectedKey.id, "ContentApi").then(data => { setLinks(data); });
+  }
+
   useEffect(() => { loadData() }, [props.arrangement]) //eslint-disable-line react-hooks/exhaustive-deps
-  useEffect(() => { loadPraiseCharts() }, [selectedKey, props.songDetail]) //eslint-disable-line react-hooks/exhaustive-deps
+  useEffect(() => { loadPraiseCharts(); loadLinks(); }, [selectedKey, props.songDetail]) //eslint-disable-line react-hooks/exhaustive-deps
 
   //<DisplayBox headerText="Keys" headerIcon="music_note">
   //<PraiseChartsProducts praiseChartsId={songDetail?.praiseChartsId} />
@@ -60,23 +75,27 @@ export const Keys = (props: Props) => {
     PraiseChartsHelper.download(skus, product.name + "." + product.file_type, keys)
   }
 
-  const listProducts = () => {
-    if (products.length === 0) return <p>No products found for this key</p>;
-    console.log("Products", products);
-    return (<ul>
-      {products.map((p, i) => (<li key={i}>
-        <a href="about:blank" onClick={(e) => { e.preventDefault(); download(p); }}>
-          {p.name}
-        </a>
-      </li>))}
+  const listProducts = () => (<ul>
+    {products.map((p, i) => (<li key={i}>
+      <a href="about:blank" onClick={(e) => { e.preventDefault(); download(p); }}>
+        {p.name}
+      </a>
+    </li>))}
+  </ul>)
 
-    </ul>)
-  }
+  const listLinks = () => (<ul>
+    {links.map((l, i) => (<li key={l.id}>
+      <a href="about:blank" onClick={(e) => { e.preventDefault(); setEditLink(l); }}><Icon>edit</Icon></a>
+      <a href={l.url} target="_blank" rel="noreferrer">{l.text}</a>
+    </li>))}
+  </ul>)
+
+
 
 
   if (editKey) return <KeyEdit arrangementKey={editKey} onSave={(k) => { setEditKey(null); loadData(); }} onCancel={() => setEditKey(null)} />
   return (<>
-    <DisplayBox headerText="Keys" headerIcon="music_note">
+    <DisplayBox headerText="Keys" headerIcon="music_note" editFunction={(selectedKey) ? () => { setEditKey(selectedKey) } : null}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={selectedKey?.id || ""} onChange={handleTabChange} aria-label="basic tabs example">
           {keys.map((k, i) => <Tab key={k.id} value={k.id} label={k.shortDescription + " (" + k.keySignature + ")"} />)}
@@ -85,8 +104,22 @@ export const Keys = (props: Props) => {
       </Box>
       {canImportLyrics && <Alert style={{ marginTop: 10 }} icon={<Button onClick={() => { props.importLyrics(); setShowImport(false); }} variant="contained" color="success">Import</Button>} severity="success">Would you like to import the text lyrics from PraiseCharts?</Alert>}
       {listProducts()}
-      <a href="about:blank" onClick={(e) => { e.preventDefault(); setShowImport(true); }}><i className="material-icons">cloud_download</i> Import files from PraiseCharts</a>
+      {listLinks()}
+
+      <Button id="addBtnGroup" variant="contained" size="small" color="success" onClick={handleClick} sx={{ mt: 2 }}>
+        <Icon sx={{ mr: "3px" }}>add</Icon> Add Files
+      </Button>
+      <Menu id="add-menu" MenuListProps={{ "aria-labelledby": "addBtnGroup" }} anchorEl={anchorEl} open={open} onClose={handleClose}>
+        <MenuItem data-cy="add-campus" onClick={() => { handleClose(); setShowImport(true); }}>
+          <Icon sx={{ mr: "3px" }}>cloud_download</Icon> Import files from PraiseCharts
+        </MenuItem>
+        <MenuItem data-cy="add-campus" onClick={() => { handleClose(); setEditLink({ category: "arrangementKey_" + selectedKey.id, linkType: "url", sort: 1, linkData: "", icon: "" }) }}>
+          <Icon sx={{ mr: "3px" }}>link</Icon> Add external link
+        </MenuItem>
+      </Menu>
+
     </DisplayBox>
+    {editLink && <LinkEdit link={editLink} onSave={(l) => { setEditLink(null); loadLinks(); }} onCancel={() => setEditLink(null)} />}
 
     {showImport && <PraiseChartsProducts praiseChartsId={props.songDetail?.praiseChartsId} keySignature={selectedKey?.keySignature || ""} onHide={() => { setShowImport(false); loadPraiseCharts(); }} />}
 
