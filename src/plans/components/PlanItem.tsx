85fca9a892f20dd1b27fa2988e8fbd4db7626471
Jeremy Zongker <jeremy@zongker.net>
2025-04-04 19:47:40 -0500

    Can add arrangementKeys to set list

--- src/plans/components/PlanItem.tsx
+++ src/plans/components/PlanItem.tsx
@@ -8,8 +8,8 @@ import { ApiHelper } from "@churchapps/apphelper";
 interface Props {
   planItem: PlanItemInterface,
   showItemDrop?: boolean,
-  onDragChange?: (isDragging:boolean) => void,
-  setEditPlanItem: (pi:PlanItemInterface) => void
+  onDragChange?: (isDragging: boolean) => void,
+  setEditPlanItem: (pi: PlanItemInterface) => void
   onChange?: () => void
 }
 
@@ -23,12 +23,12 @@ export const PlanItem = (props: Props) => {
 
   const addSong = () => {
     handleClose();
-    props.setEditPlanItem({ itemType: "song", planId: props.planItem.planId, sort: props.planItem.children?.length + 1 || 1, parentId:props.planItem.id });
+    props.setEditPlanItem({ itemType: "arrangementKey", planId: props.planItem.planId, sort: props.planItem.children?.length + 1 || 1, parentId: props.planItem.id });
   }
 
   const addItem = () => {
     handleClose();
-    props.setEditPlanItem({ itemType: "item", planId: props.planItem.planId, sort: props.planItem.children?.length + 1 || 1, parentId:props.planItem.id });
+    props.setEditPlanItem({ itemType: "item", planId: props.planItem.planId, sort: props.planItem.children?.length + 1 || 1, parentId: props.planItem.id });
   }
 
 
@@ -43,11 +43,11 @@ export const PlanItem = (props: Props) => {
   }
 
   const getChildren = () => {
-    const result:JSX.Element[] = [];
+    const result: JSX.Element[] = [];
     props.planItem.children?.forEach((c, index) => {
       result.push(
         <>
-          {props.showItemDrop && <DroppableWrapper accept="planItem" onDrop={(item) => { handleDrop(item, index+0.5)}}>&nbsp;</DroppableWrapper>}
+          {props.showItemDrop && <DroppableWrapper accept="planItem" onDrop={(item) => { handleDrop(item, index + 0.5) }}>&nbsp;</DroppableWrapper>}
 
           <DraggableWrapper dndType="planItem" data={c} draggingCallback={(isDragging) => props.onDragChange(isDragging)}>
             <PlanItem key={c.id} planItem={c} setEditPlanItem={props.setEditPlanItem} />
@@ -55,28 +55,28 @@ export const PlanItem = (props: Props) => {
         </>
       );
     });
-    if (props.showItemDrop) result.push(<DroppableWrapper accept="planItem" onDrop={(item) => { handleDrop(item, props.planItem.children?.length + 1)}}>&nbsp;</DroppableWrapper>)
+    if (props.showItemDrop) result.push(<DroppableWrapper accept="planItem" onDrop={(item) => { handleDrop(item, props.planItem.children?.length + 1) }}>&nbsp;</DroppableWrapper>)
     return result;
   }
 
   const getHeaderRow = () => <>
     <div className="planItemHeader">
-      <span style={{float:"right", marginTop:-2, marginBottom:-2}}>
+      <span style={{ float: "right", marginTop: -2, marginBottom: -2 }}>
         <a href="about:blank" onClick={e => { e.preventDefault(); setAnchorEl(e.currentTarget); }}><Icon>add</Icon></a>
-          &nbsp;
+        &nbsp;
         <a href="about:blank" onClick={e => { e.preventDefault(); props.setEditPlanItem(props.planItem); }}><Icon>edit</Icon></a>
       </span>
-      <Icon style={{float:"left", color:"#777"}}>drag_indicator</Icon> <span>{props.planItem.label}</span>
+      <Icon style={{ float: "left", color: "#777" }}>drag_indicator</Icon> <span>{props.planItem.label}</span>
     </div>
     {getChildren()}
   </>
 
   const getItemRow = () => <>
     <div className="planItem">
-      <Icon style={{float:"left", color:"#777"}}>drag_indicator</Icon>
+      <Icon style={{ float: "left", color: "#777" }}>drag_indicator</Icon>
       <div>{formatTime(props.planItem.seconds)}</div>
       <div>
-        <span style={{float:"right", marginTop:-2, marginBottom:-2}}>
+        <span style={{ float: "right", marginTop: -2, marginBottom: -2 }}>
           <a href="about:blank" onClick={e => { e.preventDefault(); props.setEditPlanItem(props.planItem); }}><Icon>edit</Icon></a>
         </span>
         {props.planItem.label}
@@ -87,10 +87,10 @@ export const PlanItem = (props: Props) => {
 
   const getSongRow = () => <>
     <div className="planItem">
-      <Icon style={{float:"left", color:"#777"}}>drag_indicator</Icon>
+      <Icon style={{ float: "left", color: "#777" }}>drag_indicator</Icon>
       <div>{formatTime(props.planItem.seconds)}</div>
       <div>
-        <span style={{float:"right", marginTop:-2, marginBottom:-2}}>
+        <span style={{ float: "right", marginTop: -2, marginBottom: -2 }}>
           <a href="about:blank" onClick={e => { e.preventDefault(); props.setEditPlanItem(props.planItem); }}><Icon>edit</Icon></a>
         </span>
         {props.planItem.label}
@@ -104,12 +104,14 @@ export const PlanItem = (props: Props) => {
   const getPlanItem = () => {
     switch (props.planItem.itemType) {
       case "header": return getHeaderRow();
-      case "song": return getSongRow();
+      case "song":
+      case "arrangementKey":
+        return getSongRow();
       case "item": return getItemRow();
     }
   }
 
-  const formatTime = (seconds:number) => {
+  const formatTime = (seconds: number) => {
     let minutes = Math.floor(seconds / 60);
     let secs = seconds % 60;
     return minutes + ":" + (secs < 10 ? "0" : "") + secs;
@@ -117,9 +119,9 @@ export const PlanItem = (props: Props) => {
 
   return (<>
     {getPlanItem()}
-    {props.planItem?.itemType==="header" && <Menu id="header-menu" anchorEl={anchorEl} open={open} onClose={handleClose}>
-      <MenuItem onClick={addSong}><Icon style={{marginRight:10}}>music_note</Icon> Song</MenuItem>
-      <MenuItem onClick={addItem}><Icon style={{marginRight:10}}>format_list_bulleted</Icon> Item</MenuItem>
+    {props.planItem?.itemType === "header" && <Menu id="header-menu" anchorEl={anchorEl} open={open} onClose={handleClose}>
+      <MenuItem onClick={addSong}><Icon style={{ marginRight: 10 }}>music_note</Icon> Song</MenuItem>
+      <MenuItem onClick={addItem}><Icon style={{ marginRight: 10 }}>format_list_bulleted</Icon> Item</MenuItem>
     </Menu>}
 
   </>)
