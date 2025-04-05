fbdd188adce3d472c434516a69b03b8c059c5efa
Jeremy Zongker <jeremy@zongker.net>
2025-04-04 20:06:35 -0500

    Cleaned up links

--- src/plans/songs/components/SongDetailLinksEdit.tsx
+++ src/plans/songs/components/SongDetailLinksEdit.tsx
@@ -1,51 +1,36 @@
 import React, { useEffect } from "react";
-import { ApiHelper, SmallButton } from "@churchapps/apphelper";
+import { ApiHelper, DisplayBox, InputBox, SmallButton } from "@churchapps/apphelper";
 import { SongDetailLinkInterface } from "../../../helpers";
 import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Table, TableBody, TableCell, TableHead, TableRow, TextField } from "@mui/material";
+import { Api } from "@mui/icons-material";
 
 interface Props {
   songDetailId: string;
-  pendingSave?: number;
-  onSave?: (songDetailLinks: SongDetailLinkInterface[]) => void;
 }
 
 export const SongDetailLinksEdit = (props: Props) => {
   const [songDetailLinks, setSongDetailLinks] = React.useState<SongDetailLinkInterface[]>([]);
-  const [pendingDeleteIds, setPendingDeleteIds] = React.useState<string[]>([]);
+  const [editLink, setEditLink] = React.useState<SongDetailLinkInterface>(null);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement> | SelectChangeEvent<string>, index: number) => {
-    const links = [...songDetailLinks];
-    const l = links[index];
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement> | SelectChangeEvent<string>) => {
+    const l = { ...editLink };
     switch (e.target.name) {
       case "serviceKey": l.serviceKey = e.target.value; break;
       case "service": l.service = e.target.value; break;
     }
     console.log(l);
-    setSongDetailLinks(links);
+    setEditLink(l);
+
   }
 
-  useEffect(() => {
-    if (props.pendingSave > 0) {
-      pendingDeleteIds.forEach(id => {
-        ApiHelper.delete("/songDetailLinks/" + id, "ContentApi");
-      });
-
-      songDetailLinks.forEach(link => { link.url = determineUrl(link); });
-
-
-      ApiHelper.post("/songDetailLinks", songDetailLinks, "ContentApi").then(() => {
-        if (props.onSave) props.onSave(songDetailLinks);
-      });
-    }
-  }, [props.pendingSave]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  useEffect(() => {
+  const loadData = () => {
     if (props.songDetailId) {
       ApiHelper.get("/songDetailLinks/songDetail/" + props.songDetailId, "ContentApi").then(data => {
         setSongDetailLinks(data);
       });
     }
-  }, [props.songDetailId]); // eslint-disable-line react-hooks/exhaustive-deps
+  }
+  useEffect(() => { loadData(); }, [props.songDetailId]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const determineUrl = (link: SongDetailLinkInterface) => {
     let result = "";
@@ -67,13 +52,22 @@ export const SongDetailLinksEdit = (props: Props) => {
     setSongDetailLinks(links);
   }
 
-  const handleDelete = (idx: number) => {
-    const links = [...songDetailLinks];
-    const toDelete = links.splice(idx, 1);
-    if (toDelete[0].id) setPendingDeleteIds([...pendingDeleteIds, toDelete[0].id]);
-    setSongDetailLinks(links);
+  const handleDelete = () => {
+    ApiHelper.delete("/songDetailLinks/" + editLink.id, "ContentApi");
+    setEditLink(null);
   }
 
+  const handleSave = () => {
+    const l = { ...editLink };
+    l.url = determineUrl(l);
+
+    ApiHelper.post("/songDetailLinks", [l], "ContentApi").then(() => {
+      loadData();
+      setEditLink(null);
+    });
+  }
+
+  /*
   const getRow = (link: SongDetailLinkInterface, idx: number) => <TableRow>
     <TableCell>
       <FormControl fullWidth size="small">
@@ -92,21 +86,40 @@ export const SongDetailLinksEdit = (props: Props) => {
     <TableCell><TextField size="small" name="serviceKey" fullWidth label="Id" value={link.serviceKey} onChange={e => handleChange(e, idx)} /></TableCell>
     <TableCell><SmallButton icon="delete" onClick={() => handleDelete(idx)} /></TableCell>
   </TableRow>
+  */
+  const getRow = (link: SongDetailLinkInterface, idx: number) => <TableRow>
+    <TableCell><a href="about:blank" onClick={(e) => { e.preventDefault(); setEditLink(link); }}>{link.service}</a></TableCell>
+    <TableCell>{link.serviceKey}</TableCell>
+  </TableRow>
 
-  return <>
-    <hr />
-    <h4>Links</h4>
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Service</TableCell>
-          <TableCell>Key</TableCell>
-          <TableCell style={{ textAlign: "right" }}><SmallButton icon="add" onClick={handleAdd} /></TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {songDetailLinks?.map((sd, i) => getRow(sd, i))}
-      </TableBody>
-    </Table>
+  if (editLink) return <InputBox headerText="Links" headerIcon="link" cancelFunction={() => { setEditLink(null) }} saveFunction={handleSave} deleteFunction={(editLink.id) ? handleDelete : null}>
+    <FormControl fullWidth size="small">
+      <InputLabel>Service</InputLabel>
+      <Select size="small" name="service" label="Service" value={editLink.service} onChange={handleChange}>
+        <MenuItem value="Apple">Apple</MenuItem>
+        <MenuItem value="CCLI">CCLI</MenuItem>
+        <MenuItem value="Genius">Genius</MenuItem>
+        <MenuItem value="Hymnary">Hymnary</MenuItem>
+        <MenuItem value="MusicBrainz">MusicBrainz</MenuItem>
+        <MenuItem value="Spotify">Spotify</MenuItem>
+        <MenuItem value="YouTube">YouTube</MenuItem>
+      </Select>
+    </FormControl>
+    <TextField size="small" name="serviceKey" fullWidth label="Id" value={editLink.serviceKey} onChange={handleChange} />
+  </InputBox>
+  else return <>
+    <DisplayBox headerText="Links" headerIcon="link" editContent={<SmallButton icon="add" onClick={handleAdd} />}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Service</TableCell>
+            <TableCell>Key</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {songDetailLinks?.map((sd, i) => getRow(sd, i))}
+        </TableBody>
+      </Table>
+    </DisplayBox>
   </>
 }
--- src/plans/songs/components/SongDetailsEdit.tsx
+++ src/plans/songs/components/SongDetailsEdit.tsx
@@ -12,13 +12,12 @@ interface Props {
 
 export const SongDetailsEdit = (props: Props) => {
   const [songDetail, setSongDetail] = React.useState<SongDetailInterface>(props.songDetail);
-  const [pendingLinksSave, setPendingLinksSave] = React.useState<number>(0);
 
   useEffect(() => { setSongDetail(props.songDetail); }, [props.songDetail]);
 
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const sd = {...songDetail};
+    const sd = { ...songDetail };
     switch (e.target.name) {
       case "album": sd.album = e.target.value; break;
       case "language": sd.language = e.target.value; break;
@@ -31,22 +30,21 @@ export const SongDetailsEdit = (props: Props) => {
   }
 
   const handleSave = () => {
-    setPendingLinksSave(pendingLinksSave + 1);
-  }
-
-  const continueSave = (songDetailLinks: SongDetailLinkInterface[]) => {
     ApiHelper.post("/songDetails", [songDetail], "ContentApi").then(data => {
       props.onSave(data[0]);
     });
   }
 
-  return (<InputBox headerText={props.songDetail?.title} headerIcon="album" saveFunction={handleSave} cancelFunction={props.onCancel}>
-    <TextField label="Album" name="album" value={songDetail?.album} onChange={handleChange} fullWidth size="small" />
-    <TextField label="Language" name="language" value={songDetail?.language} onChange={handleChange} fullWidth size="small" />
-    <TextField type="date" label="Release Date" name="releaseDate" value={songDetail?.releaseDate ? DateHelper.formatHtml5Date(songDetail.releaseDate) : ""} onChange={handleChange} fullWidth size="small" />
-    <TextField type="number" label="BPM" name="bpm" value={songDetail?.bpm} onChange={handleChange} fullWidth size="small" />
-    <TextField label="Key" name="keySignature" value={songDetail?.keySignature} placeholder="C#" onChange={handleChange} fullWidth size="small" />
-    <TextField type="number" label="Seconds" name="seconds" value={songDetail?.seconds} onChange={handleChange} fullWidth size="small" />
-    <SongDetailLinksEdit songDetailId={songDetail?.id} pendingSave={pendingLinksSave} onSave={continueSave} />
-  </InputBox>);
+
+  return (<>
+    <InputBox headerText={props.songDetail?.title} headerIcon="album" saveFunction={handleSave} cancelFunction={props.onCancel}>
+      <TextField label="Album" name="album" value={songDetail?.album} onChange={handleChange} fullWidth size="small" />
+      <TextField label="Language" name="language" value={songDetail?.language} onChange={handleChange} fullWidth size="small" />
+      <TextField type="date" label="Release Date" name="releaseDate" value={songDetail?.releaseDate ? DateHelper.formatHtml5Date(songDetail.releaseDate) : ""} onChange={handleChange} fullWidth size="small" />
+      <TextField type="number" label="BPM" name="bpm" value={songDetail?.bpm} onChange={handleChange} fullWidth size="small" />
+      <TextField label="Key" name="keySignature" value={songDetail?.keySignature} placeholder="C#" onChange={handleChange} fullWidth size="small" />
+      <TextField type="number" label="Seconds" name="seconds" value={songDetail?.seconds} onChange={handleChange} fullWidth size="small" />
+    </InputBox>
+    <SongDetailLinksEdit songDetailId={songDetail?.id} />
+  </>);
 }
