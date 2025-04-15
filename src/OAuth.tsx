9f8cadaeae93831139e7ae853d40b327515d96bc
Jeremy Zongker <jeremy@zongker.net>
2025-04-15 09:29:30 -0500

    Added oAuth page

---
+++ src/OAuth.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { useLocation, Navigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { ApiHelper, UserHelper } from "@churchapps/apphelper";
+import UserContext from "./UserContext";
+import { LoginPage, Permissions } from "@churchapps/apphelper";
+import { Alert, Box, Button, Grid, Icon } from "@mui/material";
+
+export const OAuthPage: React.FC = (props: any) => {
+  const [errors] = React.useState<string[]>([])
+  const [cookies] = useCookies();
+  const location = useLocation();
+  const [clientName, setClientName] = React.useState<string>("");
+
+  const context = React.useContext(UserContext);
+
+  let search = new URLSearchParams(window.location.search);
+  const clientId = search.get("client_id");
+  const redirectUri = search.get("redirect_uri");
+  const scope = search.get("scope");
+  const responseType = search.get("response_type");
+
+  React.useEffect(() => {
+    if (clientId) {
+      ApiHelper.get(`/oauth/clients/clientId/${clientId}`, "MembershipApi").then(client => {
+        if (client) setClientName(client.name);
+      });
+    }
+  }, [clientId]);
+
+  return (<Box sx={{ display: "flex", backgroundColor: "#EEE", minHeight: "100vh" }}>
+    <div style={{ marginLeft: "auto", marginRight: "auto", paddingTop: 20 }}>
+      <Box sx={{ width: 500, minHeight: 100, backgroundColor: "#FFF", border: "1px solid #CCC", borderRadius: "5px", padding: "10px" }} px="16px" mx="auto">
+
+        <div style={{ textAlign: "center", margin: 50 }}><img src={"/images/logo-login.png"} alt="logo" /></div>
+        <Alert severity="info" style={{ fontWeight: "bold" }}>AUTHORIZATION REQUIRED</Alert>
+        <div style={{ marginLeft: 50, marginRight: 50 }}>
+          <div style={{ textAlign: "center" }}>
+
+            <Icon style={{ fontSize: 120, marginTop: 30, color: "#777" }}>lock</Icon>
+            <h2>{clientName || "Loading..."}</h2>
+            <p>Would you like to access the following data from <b>{UserHelper.currentUserChurch.church.name}</b> in the above application?</p>
+          </div>
+
+          <ul>
+            <li>Plans</li>
+          </ul>
+        </div>
+        <div style={{ backgroundColor: "rgb(229, 246, 253)", padding: 10 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={6} style={{ textAlign: "center" }}>
+              <Button fullWidth variant="contained" color="error" onClick={() => { }}>Deny</Button>
+            </Grid>
+            <Grid item xs={6} style={{ textAlign: "center" }}>
+              <Button fullWidth variant="contained" color="primary" onClick={() => { }}>Allow</Button>
+            </Grid>
+          </Grid>
+        </div>
+
+
+      </Box>
+    </div>
+  </Box>);
+};
