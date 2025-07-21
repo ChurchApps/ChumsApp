a2cce2ec6cf15530d57f736568a95d00776807ac
Jeremy Zongker <jeremy@zongker.net>
2025-07-20 20:35:21 -0500

    Added demo react-query sample

---
+++ types/jsx.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="react" />
+
+declare global {
+  namespace JSX {
+    interface Element extends React.ReactElement<any, any> {}
+    interface ElementClass extends React.Component<any> {
+      render(): React.ReactNode;
+    }
+    interface ElementAttributesProperty {
+      props: {};
+    }
+    interface ElementChildrenAttribute {
+      children: {};
+    }
+    interface IntrinsicAttributes extends React.Attributes {}
+    interface IntrinsicClassAttributes<T> extends React.ClassAttributes<T> {}
+    interface IntrinsicElements extends React.JSX.IntrinsicElements {}
+  }
+}
+
+export {};
