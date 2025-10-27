1321f666330171bedfe2caa6ddf81bf271110f98
Kat Zongker <katrina@zongker.net>
2025-10-27 00:59:41 -0500

    attendanceDonationsTests

--- tests/attendance.spec.ts
+++ tests/attendance.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { login } from './helpers/auth';
 
+// OCTAVIAN/OCTAVIUS are the names used for testing. If you see Octavian or Octavius entered anywhere, it is a result of these tests.
 test.describe('Attendance Management', () => {
   test.beforeEach(async ({ page }) => {
     await login(page);
@@ -16,9 +17,265 @@ test.describe('Attendance Management', () => {
     await expect(page).toHaveURL(/\/attendance/);
   });
 
-  test('should load attendance page', async ({ page }) => {
+  /* test('should load attendance page', async ({ page }) => {
     const attendanceHeader = page.locator('h4').getByText('Attendance');
     await attendanceHeader.click();
+  }); */
+
+  test('should add campus', async ({ page }) => {
+    const addBtn = page.locator('[data-testid="add-campus-button"]');
+    await addBtn.click();
+    const campusName = page.locator('input[id="name"]');
+    await campusName.fill('Octavian Test Campus');
+    const saveBtn = page.locator('button').getByText('Save');
+    await saveBtn.click();
+    await page.waitForTimeout(500);
+    const verifiedName = page.locator('button').getByText('Octavian Test Campus');
+    await expect(verifiedName).toHaveCount(1);
+  });
+
+  test('should cancel adding campus', async ({ page }) => {
+    const addBtn = page.locator('[data-testid="add-campus-button"]');
+    await addBtn.click();
+    const campusName = page.locator('input[id="name"]');
+    await expect(campusName).toHaveCount(1);
+    const cancelBtn = page.locator('button').getByText('Cancel');
+    await cancelBtn.click();
+    await expect(campusName).toHaveCount(0);
+  });
+
+  test('should edit campus', async ({ page }) => {
+    const originName = page.locator('button').getByText('Octavian Test Campus');
+    await originName.click();
+    const campusName = page.locator('input[id="name"]');
+    await campusName.fill('Octavius Test Campus');
+    const saveBtn = page.locator('button').getByText('Save');
+    await saveBtn.click();
+    await page.waitForTimeout(500);
+    const verifiedName = page.locator('button').getByText('Octavius Test Campus');
+    await expect(verifiedName).toHaveCount(1);
+  });
+
+  test('should cancel editing campus', async ({ page }) => {
+    const originName = page.locator('button').getByText('Octavius Test Campus');
+    await originName.click();
+    const campusName = page.locator('input[id="name"]');
+    await expect(campusName).toHaveCount(1);
+    const cancelBtn = page.locator('button').getByText('Cancel');
+    await cancelBtn.click();
+    await expect(campusName).toHaveCount(0);
+  });
+
+  test('should add service', async ({ page }) => {
+    const addServBtn = page.locator('button').getByText('Add Service').nth(3);
+    await addServBtn.click();
+    const campusSelect = page.locator('div[role="combobox"]');
+    await campusSelect.click();
+    const selCampus = page.locator('li').getByText('Octavius Test Campus');
+    await selCampus.click();
+    const servName = page.locator('input[id="name"]');
+    await servName.fill('Octavian Test Service');
+    const saveBtn = page.locator('button').getByText('Save');
+    await saveBtn.click();
+    const verifiedServ = page.locator('button').getByText('Octavian Test Service');
+    await expect(verifiedServ).toHaveCount(1);
+  });
+
+  test('should edit service', async ({ page }) => {
+    const serv = page.locator('button').getByText('Octavian Test Service');
+    await serv.click();
+    const campusSelect = page.locator('div[role="combobox"]');
+    await campusSelect.click();
+    const selCampus = page.locator('li').getByText('Octavius Test Campus');
+    await selCampus.click();
+    const servName = page.locator('input[id="name"]');
+    await servName.fill('Octavius Test Service');
+    const saveBtn = page.locator('button').getByText('Save');
+    await saveBtn.click();
+    await page.waitForTimeout(500);
+    const verifiedServ = page.locator('button').getByText('Octavius Test Service');
+    await expect(verifiedServ).toHaveCount(1);
+  });
+
+  test('should cancel editing service', async ({ page }) => {
+    const serv = page.locator('button').getByText('Sunday Evening Service');
+    await serv.click();
+    const campusSelect = page.locator('div[role="combobox"]');
+    await expect(campusSelect).toHaveCount(1);
+    const cancelBtn = page.locator('button').getByText('Cancel');
+    await cancelBtn.click();
+    await page.waitForTimeout(500);
+    await expect(campusSelect).toHaveCount(0);
+  });
+
+  test('should add service time', async ({ page }) => {
+    const addServTimeBtn = page.locator('button').getByText('Add Service Time').first();
+    await addServTimeBtn.click();
+    const servSelect = page.locator('div[role="combobox"]');
+    await servSelect.click();
+    const selServ = page.locator('li').getByText('Octavius Test Service');
+    await selServ.click();
+    const timeName = page.locator('input[id="name"]');
+    await timeName.fill('Octavian Test Time');
+    const saveBtn = page.locator('button').getByText('Save');
+    await saveBtn.click();
+    const verifiedTime = page.locator('button').getByText('Octavian Test Time');
+    await expect(verifiedTime).toHaveCount(1);
+  });
+
+  test('should edit service time', async ({ page }) => {
+    const time = page.locator('button').getByText('Octavian Test Time');
+    await time.click();
+    const servSelect = page.locator('div[role="combobox"]');
+    await servSelect.click();
+    const selServ = page.locator('li').getByText('Octavius Test Service');
+    await selServ.click();
+    const timeName = page.locator('input[id="name"]');
+    await timeName.fill('Octavius Test Time');
+    const saveBtn = page.locator('button').getByText('Save');
+    await saveBtn.click();
+    await page.waitForTimeout(500);
+    const verifiedTime = page.locator('button').getByText('Octavius Test Time');
+    await expect(verifiedTime).toHaveCount(1);
+  });
+
+  test('should cancel editing service time', async ({ page }) => {
+    const serv = page.locator('button').getByText('6:00 PM Service');
+    await serv.click();
+    const servSelect = page.locator('div[role="combobox"]');
+    await expect(servSelect).toHaveCount(1);
+    const cancelBtn = page.locator('button').getByText('Cancel');
+    await cancelBtn.click();
+    await page.waitForTimeout(500);
+    await expect(servSelect).toHaveCount(0);
+  });
+
+  test('should delete service time', async ({ page }) => {
+    page.once('dialog', async dialog => {
+      expect(dialog.type()).toBe('confirm');
+      expect(dialog.message()).toContain('Are you sure');
+      await dialog.accept();
+    });
+
+    const time = page.locator('button').getByText('Octavius Test Time');
+    await time.click();
+    const deleteBtn = page.locator('button').getByText('Delete');
+    await deleteBtn.click();
+    await page.waitForTimeout(500);
+    await expect(time).toHaveCount(0);
+  });
+
+  test('UPDATE should delete service', async ({ page }) => {
+    page.once('dialog', async dialog => {
+      expect(dialog.type()).toBe('confirm');
+      //update delete dialogue v
+      expect(dialog.message()).toContain('Service name');
+      await dialog.accept();
+    });
+
+    const serv = page.locator('button').getByText('Octavius Test Service');
+    await serv.click();
+    const deleteBtn = page.locator('button').getByText('Delete');
+    await deleteBtn.click();
+    await page.waitForTimeout(500);
+    await expect(serv).toHaveCount(0);
+  });
+
+  test('should delete campus', async ({ page }) => {
+    page.once('dialog', async dialog => {
+      expect(dialog.type()).toBe('confirm');
+      expect(dialog.message()).toContain('Are you sure');
+      await dialog.accept();
+    });
+
+    const originName = page.locator('button').getByText('Octavius Test Campus');
+    await originName.click();
+    const deleteBtn = page.locator('button').getByText('Delete');
+    await deleteBtn.click();
+    await expect(originName).toHaveCount(0);
+  });
+
+  test('should view group from attendance homepage', async ({ page }) => {
+    const groupBtn = page.locator('a').getByText('Preschool (3-5)').first();
+    await groupBtn.click();
+
+    await page.waitForURL(/\/groups\/GRP\d+/, { timeout: 10000 });
+    await expect(page).toHaveURL(/\/groups\/GRP\d+/);
+
+    const groupHeader = page.locator('p').getByText('Preschool (3-5)').first();
+    await groupHeader.click();
+  });
+
+  test('should filter attendance trends', async ({ page }) => {
+    const trendTab = page.locator('button[role="tab"]').getByText('Attendance Trend');
+    await trendTab.click();
+    await page.waitForTimeout(500);
+
+    const campusName = page.locator('[id="mui-component-select-campusId"]');
+    await campusName.click();
+    const campusSel = page.locator('li').getByText('Main Campus');
+    await campusSel.click();
+    const serviceName = page.locator('[id="mui-component-select-serviceId"]');
+    await serviceName.click();
+    const serviceSel = page.locator('li').getByText('Sunday Morning Service');
+    await serviceSel.click();
+    const timeName = page.locator('[id="mui-component-select-serviceTimeId"]');
+    await timeName.click();
+    const timeSel = page.locator('li').getByText('10:30 AM Service');
+    await timeSel.click();
+    const groupName = page.locator('[id="mui-component-select-groupId"]');
+    await groupName.click();
+    const groupSel = page.locator('li').getByText('Sunday Morning Service');
+    await groupSel.click();
+    const runBtn = page.locator('button').getByText('Run Report');
+    await runBtn.click();
+    await page.waitForTimeout(500);
+
+    const resultsTableRows = page.locator('[id="reportsBox"] table tr');
+    expect(resultsTableRows).toHaveCount(36);
+  });
+
+  test('UNFINISHED should print attendance trends', async ({ page }) => {
+    const trendTab = page.locator('button[role="tab"]').getByText('Attendance Trend');
+    await trendTab.click();
+    await page.waitForTimeout(500);
+
+    const printBtn = page.locator('button').getByText('print');
+    await printBtn.click();
+    await page.waitForTimeout(500);
+  });
+
+  test('UNFINISHED should download attendance trends', async ({ page }) => {
+    const trendTab = page.locator('button[role="tab"]').getByText('Attendance Trend');
+    await trendTab.click();
+    await page.waitForTimeout(500);
+
+    const downloadBtn = page.locator('button').getByText('download');
+    await downloadBtn.click();
+    await page.waitForTimeout(500);
+  });
+
+  test('UNFINISHED should display group attendance', async ({ page }) => {
+    // completed as I can, correcting reports display info is up to father.
+    const trendTab = page.locator('button[role="tab"]').getByText('Group Attendance');
+    await trendTab.click();
+    await page.waitForTimeout(500);
+
+    const campusName = page.locator('[id="mui-component-select-campusId"]');
+    await campusName.click();
+    const campusSel = page.locator('li').getByText('Main Campus');
+    await campusSel.click();
+    const serviceName = page.locator('[id="mui-component-select-serviceId"]');
+    await serviceName.click();
+    const serviceSel = page.locator('li').getByText('Sunday Morning Service');
+    await serviceSel.click();
+    const weekBox = page.locator('[name="week"]');
+    await weekBox.fill('2024-03-03');
+    const runBtn = page.locator('button').getByText('Run Report');
+    await runBtn.click();
+    await page.waitForTimeout(500);
+    const report = page.locator('td').getByText('10:30 AM Service');
+    await report.click();
   });
 
 });
---
+++ tests/donations.spec.ts
@@ -0,0 +1,231 @@
+import { test, expect } from '@playwright/test';
+import { login } from './helpers/auth';
+
+// OCTAVIAN/OCTAVIUS are the names used for testing. If you see Octavian or Octavius entered anywhere, it is a result of these tests.
+test.describe('Donations Management', () => {
+  test.beforeEach(async ({ page }) => {
+    await login(page);
+    const menuBtn = page.locator('[id="primaryNavButton"]').getByText('expand_more');
+    await menuBtn.click();
+    const donationHomeBtn = page.locator('[data-testid="nav-item-donations"]');
+    await donationHomeBtn.click();
+    await page.waitForTimeout(5000);
+    await expect(page).toHaveURL(/\/donations/);
+  });
+
+  /* test('should load donations page', async ({ page }) => {
+    const donationHeader = page.locator('h4').getByText('Donations');
+    await donationHeader.click();
+  }); */
+
+  test('should run donations summary', async ({ page }) => {
+    const startDate = page.locator('[name="startDate"]');
+    await startDate.fill('2025-03-01');
+    const endDate = page.locator('[name="endDate"]');
+    await endDate.fill('2025-05-01');
+    const runBtn = page.locator('button').getByText('Run Report');
+    await runBtn.click();
+
+    const verifyStart = page.locator('g text').nth(6);
+    await expect(verifyStart).toContainText('Mar 1');
+    const startYear = page.locator('g text').nth(7);
+    await expect(startYear).toContainText('2025');
+    const verifyEnd = page.locator('g text').nth(22);
+    await expect(verifyEnd).toContainText('Apr 26');
+    const endYear = page.locator('g text').nth(23);
+    await expect(endYear).toContainText('2025');
+  });
+
+  test('should create fund', async ({ page }) => {
+    const fundsBtn = page.locator('[id="secondaryMenu"]').getByText('Funds');
+    await fundsBtn.click();
+    await page.waitForTimeout(500);
+    const addBtn = page.locator('[data-testid="add-fund-button"]');
+    await addBtn.click();
+    const fundName = page.locator('[name="fundName"]');
+    await fundName.fill('Octavian Fund');
+    const taxCheck = page.locator('[name="taxDeductible"]');
+    await taxCheck.click();
+    const saveBtn = page.locator('button').getByText('Save');
+    await saveBtn.click();
+    await page.waitForTimeout(500);
+
+    const verifyFund = page.locator('a').getByText('Octavian Fund');
+    await expect(verifyFund).toHaveCount(1);
+    const verifyTax = page.locator('p').getByText('Non-Deductible');
+    await expect(verifyTax).toHaveCount(1);
+  });
+
+  test('should edit fund', async ({ page }) => {
+    const fundsBtn = page.locator('[id="secondaryMenu"]').getByText('Funds');
+    await fundsBtn.click();
+    await page.waitForTimeout(500);
+    const editBtn = page.locator('[data-cy="edit-5"]');
+    await editBtn.click();
+    const fundName = page.locator('[name="fundName"]');
+    await fundName.fill('Octavius Fund');
+    const taxCheck = page.locator('[name="taxDeductible"]');
+    await taxCheck.click();
+    const saveBtn = page.locator('button').getByText('Save');
+    await saveBtn.click();
+    await page.waitForTimeout(500);
+
+    const verifyFund = page.locator('a').getByText('Octavius Fund');
+    await expect(verifyFund).toHaveCount(1);
+    const verifyTax = page.locator('p').getByText('Non-Deductible');
+    await expect(verifyTax).toHaveCount(0);
+  });
+
+  test('should cancel editing fund', async ({ page }) => {
+    const fundsBtn = page.locator('[id="secondaryMenu"]').getByText('Funds');
+    await fundsBtn.click();
+    await page.waitForTimeout(500);
+    const editBtn = page.locator('[data-cy="edit-5"]');
+    await editBtn.click();
+    const fundName = page.locator('[name="fundName"]');
+    await expect(fundName).toHaveCount(1);
+    const cancelBtn = page.locator('button').getByText('Cancel');
+    await cancelBtn.click();
+    await page.waitForTimeout(500);
+    await expect(fundName).toHaveCount(0);
+  });
+
+  test('UPDATE should create batch', async ({ page }) => {
+    const batchesBtn = page.locator('[id="secondaryMenu"]').getByText('Batches');
+    await batchesBtn.click();
+    await page.waitForTimeout(500);
+
+    const addBtn = page.locator('[data-testid="add-batch-button"]');
+    await addBtn.click();
+    const batchName = page.locator('[name="name"]');
+    await batchName.fill('October 10, 2025 Batch');
+    //Update input to select today, not a day ahead v
+    const date = page.locator('[name="date"]');
+    await date.fill('2025-10-01');
+    const saveBtn = page.locator('button').getByText('Save');
+    await saveBtn.click();
+    await page.waitForTimeout(500);
+
+    const verifyBatch = page.locator('a').getByText('October 10, 2025 Batch');
+    await expect(verifyBatch).toHaveCount(1);
+    //Update date results to show dates, then add date checking
+  });
+
+  test('DOES NOT WORK should edit batch', async ({ page }) => {
+    const batchesBtn = page.locator('[id="secondaryMenu"]').getByText('Batches');
+    await batchesBtn.click();
+    await page.waitForTimeout(500);
+
+    const editBtn = page.locator('[data-cy="edit-0"]');
+    await editBtn.click();
+    const batchName = page.locator('[name="name"]');
+    await batchName.fill('October 1, 2025 Batch');
+    const date = page.locator('[name="date"]');
+    await date.fill('2025-10-01');
+    const saveBtn = page.locator('button').getByText('Save');
+    await saveBtn.click();
+    await page.waitForTimeout(500);
+
+    const verifyBatch = page.locator('a').getByText('October 1, 2025 Batch');
+    await expect(verifyBatch).toHaveCount(1);
+  });
+
+  test('should cancel editing batch', async ({ page }) => {
+    const batchesBtn = page.locator('[id="secondaryMenu"]').getByText('Batches');
+    await batchesBtn.click();
+    await page.waitForTimeout(500);
+
+    const editBtn = page.locator('[data-cy="edit-0"]');
+    await editBtn.click();
+    const batchName = page.locator('[name="name"]');
+    await expect(batchName).toHaveCount(1);
+    const cancelBtn = page.locator('button').getByText('Cancel');
+    await cancelBtn.click();
+    await page.waitForTimeout(500);
+    await expect(batchName).toHaveCount(0);
+  });
+
+  test('UNFINISHED should add donation to batch', async ({ page }) => {
+    const batchesBtn = page.locator('[id="secondaryMenu"]').getByText('Batches');
+    await batchesBtn.click();
+    await page.waitForTimeout(500);
+
+    const selBatch = page.locator('a').getByText('October 1, 2025 Batch');
+    await selBatch.click();
+    await page.waitForTimeout(500);
+
+    const anon = page.locator('button').getByText('Anonymous');
+    await anon.click();
+    const date = page.locator('input').first();
+    await date.fill('2025-05-02');
+    const method = page.locator('[role="combobox"]').first();
+    await method.click();
+    const methodSel = page.locator('[data-value="Cash"]');
+    await methodSel.click();
+    const fund = page.locator('[role="combobox"]').nth(1);
+    await fund.click();
+    const fundSel = page.locator('li').getByText('Octavius Fund');
+    await fundSel.click();
+    const notes = page.locator('input').nth(1);
+    await notes.fill('Test Donation Notes');
+    const amount = page.locator('input').nth(2);
+    await amount.fill('20.00');
+    const addBtn = page.locator('[data-testid="add-donation-submit"]');
+    await addBtn.click();
+  });
+
+  test('should go back to person select on donation entry', async ({ page }) => {
+    const batchesBtn = page.locator('[id="secondaryMenu"]').getByText('Batches');
+    await batchesBtn.click();
+    await page.waitForTimeout(500);
+
+    const selBatch = page.locator('a').getByText('October 1, 2025 Batch');
+    await selBatch.click();
+    await page.waitForTimeout(500);
+
+    const anon = page.locator('button').getByText('Anonymous');
+    await anon.click();
+    const change = page.locator('button').getByText('Change');
+    await change.click();
+    await anon.click();
+  });
+
+  test('DOES NOT WORK should delete batch', async ({ page }) => {
+    page.once('dialog', async dialog => {
+      expect(dialog.type()).toBe('confirm');
+      expect(dialog.message()).toContain('Are you sure');
+      await dialog.accept();
+    });
+
+    const batchesBtn = page.locator('[id="secondaryMenu"]').getByText('Batches');
+    await batchesBtn.click();
+    await page.waitForTimeout(500);
+
+    const editBtn = page.locator('[data-cy="edit-0"]');
+    await editBtn.click();
+    const deleteBtn = page.locator('[id="delete"]');
+    await deleteBtn.click();
+    await page.waitForTimeout(500);
+    const verifyBatch = page.locator('a').getByText('October 1, 2025 Batch');
+    await expect(verifyBatch).toHaveCount(0);
+  });
+
+  test('should delete fund', async ({ page }) => {
+    page.once('dialog', async dialog => {
+      expect(dialog.type()).toBe('confirm');
+      expect(dialog.message()).toContain('Are you sure');
+      await dialog.accept();
+    });
+
+    const fundsBtn = page.locator('[id="secondaryMenu"]').getByText('Funds');
+    await fundsBtn.click();
+    await page.waitForTimeout(500);
+    const editBtn = page.locator('[data-cy="edit-5"]');
+    await editBtn.click();
+    const deleteBtn = page.locator('[id="delete"]');
+    await deleteBtn.click();
+    const verifyFund = page.locator('a').getByText('Octavius Fund');
+    await expect(verifyFund).toHaveCount(0);
+  });
+
+});
