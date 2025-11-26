f26719ff6c20c195b0b353f3b20e97a4aa08b117
Jeremy Zongker <jeremy@zongker.net>
2025-11-26 11:44:36 -0600

    Added sentry

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,23 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { EnvironmentHelper } from "./helpers";
 
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://0fa8dbad4eea6ffc6b2ffc157c43cff2@o4510432524107776.ingest.us.sentry.io/4510432531251200",
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration()
+  ],
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  enableLogs: true
+});
+
+
 EnvironmentHelper.init().then(() => {
   const root = createRoot(document.getElementById("root"));
   //root.render(<React.StrictMode><App /></React.StrictMode>);
